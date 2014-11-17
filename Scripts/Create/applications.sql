2a3441bb43e0084120eefadb35349620a9b68446
gregtheyoung <github@twinarchtech.com>
2014-11-16 22:22:18 -0500

    Update raw schemas

--- Scripts/Create/applications.sql
+++ Scripts/Create/applications.sql
@@ -10,12 +10,12 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping database structure for app_smalltest_20141114
-CREATE DATABASE IF NOT EXISTS `app_smalltest_20141114` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `app_smalltest_20141114`;
+-- Dumping database structure for app_expandedtest_20141117
+CREATE DATABASE IF NOT EXISTS `app_expandedtest_20141117` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `app_expandedtest_20141117`;
 
 
--- Dumping structure for table app_smalltest_20141114.application
+-- Dumping structure for table app_expandedtest_20141117.application
 CREATE TABLE IF NOT EXISTS `application` (
   `id` varchar(36) NOT NULL,
   `type` varchar(20) DEFAULT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `application` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.application_assignee
+-- Dumping structure for table app_expandedtest_20141117.application_assignee
 CREATE TABLE IF NOT EXISTS `application_assignee` (
   `application_id` varchar(20) DEFAULT NULL,
   `assignee_id` varchar(36) DEFAULT NULL,
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `application_assignee` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.application_inventor
+-- Dumping structure for table app_expandedtest_20141117.application_inventor
 CREATE TABLE IF NOT EXISTS `application_inventor` (
   `application_id` varchar(20) DEFAULT NULL,
   `inventor_id` varchar(36) DEFAULT NULL,
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `application_inventor` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.assignee
+-- Dumping structure for table app_expandedtest_20141117.assignee
 CREATE TABLE IF NOT EXISTS `assignee` (
   `id` varchar(36) NOT NULL,
   `type` varchar(10) DEFAULT NULL,
@@ -74,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `assignee` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.claim
+-- Dumping structure for table app_expandedtest_20141117.claim
 CREATE TABLE IF NOT EXISTS `claim` (
   `uuid` varchar(36) NOT NULL,
   `application_id` varchar(20) DEFAULT NULL,
@@ -90,7 +90,62 @@ CREATE TABLE IF NOT EXISTS `claim` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.inventor
+-- Dumping structure for table app_expandedtest_20141117.cpc_current
+CREATE TABLE IF NOT EXISTS `cpc_current` (
+  `uuid` varchar(36) NOT NULL,
+  `application_id` varchar(20) DEFAULT NULL,
+  `section_id` varchar(10) DEFAULT NULL,
+  `subsection_id` varchar(20) DEFAULT NULL,
+  `group_id` varchar(20) DEFAULT NULL,
+  `subgroup_id` varchar(20) DEFAULT NULL,
+  `category` varchar(36) DEFAULT NULL,
+  `sequence` int(11) DEFAULT NULL,
+  PRIMARY KEY (`uuid`),
+  KEY `application_id` (`application_id`),
+  KEY `subsection_id` (`subsection_id`),
+  KEY `group_id` (`group_id`),
+  KEY `subgroup_id` (`subgroup_id`),
+  KEY `ix_cpc_current_sequence` (`sequence`),
+  CONSTRAINT `cpc_current_ibfk_1` FOREIGN KEY (`application_id`) REFERENCES `application` (`id`),
+  CONSTRAINT `cpc_current_ibfk_2` FOREIGN KEY (`subsection_id`) REFERENCES `cpc_subsection` (`id`),
+  CONSTRAINT `cpc_current_ibfk_3` FOREIGN KEY (`group_id`) REFERENCES `cpc_group` (`id`),
+  CONSTRAINT `cpc_current_ibfk_4` FOREIGN KEY (`subgroup_id`) REFERENCES `cpc_subgroup` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table app_expandedtest_20141117.cpc_group
+CREATE TABLE IF NOT EXISTS `cpc_group` (
+  `id` varchar(20) NOT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table app_expandedtest_20141117.cpc_subgroup
+CREATE TABLE IF NOT EXISTS `cpc_subgroup` (
+  `id` varchar(20) NOT NULL,
+  `title` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table app_expandedtest_20141117.cpc_subsection
+CREATE TABLE IF NOT EXISTS `cpc_subsection` (
+  `id` varchar(20) NOT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table app_expandedtest_20141117.inventor
 CREATE TABLE IF NOT EXISTS `inventor` (
   `id` varchar(36) NOT NULL,
   `name_first` varchar(64) DEFAULT NULL,
@@ -101,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `inventor` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.location
+-- Dumping structure for table app_expandedtest_20141117.location
 CREATE TABLE IF NOT EXISTS `location` (
   `id` varchar(128) NOT NULL,
   `city` varchar(128) DEFAULT NULL,
@@ -110,8 +165,8 @@ CREATE TABLE IF NOT EXISTS `location` (
   `latitude` float DEFAULT NULL,
   `longitude` float DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `ix_location_state` (`state`),
   KEY `dloc_idx1` (`latitude`,`longitude`),
+  KEY `ix_location_state` (`state`),
   KEY `dloc_idx2` (`city`,`state`,`country`),
   KEY `ix_location_country` (`country`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -119,7 +174,7 @@ CREATE TABLE IF NOT EXISTS `location` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.location_assignee
+-- Dumping structure for table app_expandedtest_20141117.location_assignee
 CREATE TABLE IF NOT EXISTS `location_assignee` (
   `location_id` varchar(128) DEFAULT NULL,
   `assignee_id` varchar(36) DEFAULT NULL,
@@ -132,7 +187,7 @@ CREATE TABLE IF NOT EXISTS `location_assignee` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.location_inventor
+-- Dumping structure for table app_expandedtest_20141117.location_inventor
 CREATE TABLE IF NOT EXISTS `location_inventor` (
   `location_id` varchar(128) DEFAULT NULL,
   `inventor_id` varchar(36) DEFAULT NULL,
@@ -145,7 +200,7 @@ CREATE TABLE IF NOT EXISTS `location_inventor` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.mainclass
+-- Dumping structure for table app_expandedtest_20141117.mainclass
 CREATE TABLE IF NOT EXISTS `mainclass` (
   `id` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
@@ -154,7 +209,7 @@ CREATE TABLE IF NOT EXISTS `mainclass` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.mainclass_current
+-- Dumping structure for table app_expandedtest_20141117.mainclass_current
 CREATE TABLE IF NOT EXISTS `mainclass_current` (
   `id` varchar(20) NOT NULL,
   `title` varchar(256) DEFAULT NULL,
@@ -164,7 +219,7 @@ CREATE TABLE IF NOT EXISTS `mainclass_current` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.rawassignee
+-- Dumping structure for table app_expandedtest_20141117.rawassignee
 CREATE TABLE IF NOT EXISTS `rawassignee` (
   `uuid` varchar(36) NOT NULL,
   `application_id` varchar(20) DEFAULT NULL,
@@ -188,7 +243,7 @@ CREATE TABLE IF NOT EXISTS `rawassignee` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.rawinventor
+-- Dumping structure for table app_expandedtest_20141117.rawinventor
 CREATE TABLE IF NOT EXISTS `rawinventor` (
   `uuid` varchar(36) NOT NULL,
   `application_id` varchar(20) DEFAULT NULL,
@@ -210,7 +265,7 @@ CREATE TABLE IF NOT EXISTS `rawinventor` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.rawlocation
+-- Dumping structure for table app_expandedtest_20141117.rawlocation
 CREATE TABLE IF NOT EXISTS `rawlocation` (
   `id` varchar(128) NOT NULL,
   `location_id` varchar(128) DEFAULT NULL,
@@ -220,15 +275,15 @@ CREATE TABLE IF NOT EXISTS `rawlocation` (
   PRIMARY KEY (`id`),
   KEY `location_id` (`location_id`),
   KEY `ix_rawlocation_state` (`state`),
-  KEY `ix_rawlocation_country` (`country`),
   KEY `loc_idx1` (`city`,`state`,`country`),
+  KEY `ix_rawlocation_country` (`country`),
   CONSTRAINT `rawlocation_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.subclass
+-- Dumping structure for table app_expandedtest_20141117.subclass
 CREATE TABLE IF NOT EXISTS `subclass` (
   `id` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
@@ -237,7 +292,7 @@ CREATE TABLE IF NOT EXISTS `subclass` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.subclass_current
+-- Dumping structure for table app_expandedtest_20141117.subclass_current
 CREATE TABLE IF NOT EXISTS `subclass_current` (
   `id` varchar(20) NOT NULL,
   `title` varchar(512) DEFAULT NULL,
@@ -247,7 +302,7 @@ CREATE TABLE IF NOT EXISTS `subclass_current` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.temporary_update
+-- Dumping structure for table app_expandedtest_20141117.temporary_update
 CREATE TABLE IF NOT EXISTS `temporary_update` (
   `pk` varchar(36) NOT NULL,
   `update` varchar(36) DEFAULT NULL,
@@ -258,7 +313,7 @@ CREATE TABLE IF NOT EXISTS `temporary_update` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.uspc
+-- Dumping structure for table app_expandedtest_20141117.uspc
 CREATE TABLE IF NOT EXISTS `uspc` (
   `uuid` varchar(36) NOT NULL,
   `application_id` varchar(20) DEFAULT NULL,
@@ -278,7 +333,7 @@ CREATE TABLE IF NOT EXISTS `uspc` (
 -- Data exporting was unselected.
 
 
--- Dumping structure for table app_smalltest_20141114.uspc_current
+-- Dumping structure for table app_expandedtest_20141117.uspc_current
 CREATE TABLE IF NOT EXISTS `uspc_current` (
   `uuid` varchar(36) NOT NULL,
   `application_id` varchar(20) DEFAULT NULL,
