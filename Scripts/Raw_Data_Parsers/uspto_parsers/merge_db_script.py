46f8485d23626cf188328567379fd0b16538ff62
gregtheyoung <github@twinarchtech.com>
2014-11-25 09:19:54 -0500

    Change merge script to handle the application table (it can have dups) and allow for hyphens in DB names.

--- Scripts/Raw_Data_Parsers/uspto_parsers/merge_db_script.py
+++ Scripts/Raw_Data_Parsers/uspto_parsers/merge_db_script.py
@@ -13,16 +13,16 @@ def merge_db_pats(host,username,password,sourcedb,targetdb):
     
     cursor = mydb.cursor()
     
-    primaries = ['mainclass','subclass','mainclass_current','subclass_current']
+    primaries = ['application','mainclass','subclass','mainclass_current','subclass_current']
     for d in dbnames:
         cursor.execute('SET FOREIGN_KEY_CHECKS=0')
-        cursor.execute('SHOW TABLES FROM '+d)
+        cursor.execute('SHOW TABLES FROM `'+d+'`')
         tables = [t[0] for t in cursor.fetchall()]
         for t in tables:
             print d,t
             if t in primaries:
-                cursor.execute('INSERT INTO '+targetdb+'.'+t+' SELECT * FROM '+d+'.'+t+' WHERE '+d+'.'+t+'.id NOT IN (SELECT id FROM '+targetdb+'.'+t+')')
+                cursor.execute('INSERT INTO '+targetdb+'.'+t+' SELECT * FROM `'+d+'`.'+t+' WHERE `'+d+'`.'+t+'.id NOT IN (SELECT id FROM `'+targetdb+'`.'+t+')')
             else:
-                cursor.execute('INSERT INTO '+targetdb+'.'+t+' SELECT * from '+d+'.'+t)
+                cursor.execute('INSERT INTO '+targetdb+'.'+t+' SELECT * from `'+d+'`.'+t)
             mydb.commit()
         cursor.execute('set foreign_key_checks=1')
