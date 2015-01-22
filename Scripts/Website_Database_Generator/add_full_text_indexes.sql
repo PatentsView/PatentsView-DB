f569c90d0958102edc11312affdbd4f22f88634e
Everst <Everst@users.noreply.github.com>
2015-01-22 17:15:40 -0500

    No fulltext index for class fields
    To respond to request on making class fields consistent as 'string'.

--- Scripts/Website_Database_Generator/add_full_text_indexes.sql
+++ Scripts/Website_Database_Generator/add_full_text_indexes.sql
@@ -10,9 +10,9 @@
 # 3:16:38
 alter table `PatentsView_20141215_dev`.`patent` add fulltext index `fti_patent_abstract` (`abstract`);
 alter table `PatentsView_20141215_dev`.`patent` add fulltext index `fti_patent_title` (`title`);
-alter table `PatentsView_20141215_dev`.`uspc_current` add fulltext index `fti_uspc_current_mainclass_title` (`mainclass_title`);
-alter table `PatentsView_20141215_dev`.`uspc_current` add fulltext index `fti_uspc_current_subclass_title` (`subclass_title`);
-alter table `PatentsView_20141215_dev`.`uspc_current_mainclass` add fulltext index `fti_uspc_current_mainclass_mainclass_title` (`mainclass_title`);
+# alter table `PatentsView_20141215_dev`.`uspc_current` add fulltext index `fti_uspc_current_mainclass_title` (`mainclass_title`);
+# alter table `PatentsView_20141215_dev`.`uspc_current` add fulltext index `fti_uspc_current_subclass_title` (`subclass_title`);
+# alter table `PatentsView_20141215_dev`.`uspc_current_mainclass` add fulltext index `fti_uspc_current_mainclass_mainclass_title` (`mainclass_title`);
 
 
 # END full text indexing ######################################################################################################################################
