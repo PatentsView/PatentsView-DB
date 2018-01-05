d707dba55b384e66584b1a04e370490721a7db7c
Sarvothaman Madhavan <smadhavan@air.org>
2018-01-05 13:52:32 -0500

    Updated bulk generator to use config file

--- Scripts/Raw_Database_to_Production/bulk-download-generator.py
+++ Scripts/Raw_Database_to_Production/bulk-download-generator.py
@@ -1,24 +1,25 @@
 import argparse
 import MySQLdb
 import pandas as pd
-
+import configparser
 from sqlalchemy import create_engine
 import os
 
-
-host = os.environ['PV_INJ_HOST']
-user = os.environ['PV_INJ_USERNAME']
-password = os.environ['PV_INJ_PASSWORD']
-port = os.environ['PV_INJ_PORT']
+#host = os.environ['PV_INJ_HOST']
+#user = os.environ['PV_INJ_USERNAME']
+#password = os.environ['PV_INJ_PASSWORD']
+#port = os.environ['PV_INJ_PORT']
 
 
 
 def export_table(t, db_connection=None, column_list=None):
     select_columns = ",".join(column_list) if column_list is not None else "*"
     query = "select " + select_columns + " from " + str(t)
-    filename = str(t) + ".tsv"
+    filename = "/data/"+str(t) + ".tsv"
     print(query)
+    print(filename)
     table_data = pd.read_sql(query, con=db_connection)
+    print(table_data)
     table_data.to_csv(filename, sep="\t", index=False)
 
 
@@ -35,6 +36,11 @@ def main():
         type=str,
         nargs=1,
         help='Database name (required)')
+    parser.add_argument(
+        '-c',
+        type=str,
+        nargs=1,
+        help='File containing database config in INI format')
     args = parser.parse_args()
     default_tables = """application assignee botanic brf_sum_text claim cpc_current cpc_group cpc_subsection cpc_subgroup draw_desc_text figures foreign_priority government_interest foreigncitation government_organization inventor ipcr lawyer location location_assignee location_inventor mainclass mainclass_current nber nber_category nber_subcategory non_inventor_applicant otherreference patent patent_assignee patent_contractawardnumber patent_govintorg patent_inventor patent_lawyer pct_data rawassignee rawexaminer rawinventor rawlawyer rawlocation rel_app_text subclass subclass_current us_term_of_grant usapplicationcitation uspatentcitation uspc uspc_current usreldoc wipo wipo_field""".split(
         " ")
@@ -55,16 +61,23 @@ def main():
         except:
             tables = default_tables
         tables = [line.rstrip("\n") for line in lines]
+    config = configparser.ConfigParser()
+    config.read(args.c[0])
+    print(config)
+    user=config["dev_database"]["mysql_db_user"]
+    password=config["dev_database"]["mysql_db_password"]
+    host=config["dev_database"]["mysql_db_host"]
+    port=config["dev_database"]["mysql_db_port"]
+    database=config["dev_database"]["mysql_db_name"]
+                                
     connection_string = 'mysql://' + \
         str(user) + ':' + str(password) + '@' + \
         str(host) + ':' + str(port) + '/' + str(database)
     read_engine = create_engine(connection_string,
                                 echo=True)
-    try:
-        for table in tables:
+    for table in tables:
             export_table(table, read_engine)
-    finally:
-        read_engine.dispose()
+    read_engine.dispose()
 
 if __name__ == "__main__":
     main()
