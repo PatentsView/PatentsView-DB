202f5bb8c88588b5bc0df0f22ae18e96bfcc4e27
jsennett <joshua.sennett@tufts.edu>
2018-06-18 11:36:15 -0400

    Improved xml parsing of CPC groups

---
+++ Scripts/Raw_Data_Parsers/uspto_parsers/cpc_class_tables_test.py
@@ -0,0 +1,6 @@
+from cpc_class_tables import cpc_class_tables
+
+if __name__ == '__main__':
+    outputdir = "/Users/jsennett/Documents/PatentsView/CPC_Output"
+    datadir ="/Users/jsennett/Documents/PatentsView/CPC_Input"
+    cpc_class_tables(outputdir,datadir)
