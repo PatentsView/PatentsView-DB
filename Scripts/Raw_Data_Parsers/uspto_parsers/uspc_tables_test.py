f700a08b26aeb02e89b8a223d37cc079602a5893
jsennett <joshua.sennett@tufts.edu>
2018-06-22 16:09:54 -0400

    working through parsers

--- Scripts/Raw_Data_Parsers/uspto_parsers/uspc_tables_test.py
+++ Scripts/Raw_Data_Parsers/uspto_parsers/uspc_tables_test.py
@@ -1,6 +1,6 @@
 import sys
+from uspc_table import parse_and_write_uspc, download
 
-from uspc_table import parse_and_write_uspc, download, unzip
 
 if __name__ == '__main__':
 
@@ -18,21 +18,10 @@ if __name__ == '__main__':
 
 
     # Download and extract tables
-    # for table_name in ['mcfpat.zip', 'mcfappl.zip']:
-    #     url = 'https://bulkdata.uspto.gov/'\
-    #                 'data/patent/classification/{}'.format(table_name)
-    #
-    #     # Download tables
-    #     print("Downloading: {}".format(url))
-    #     download(url, inputdir, table_name)
-    #
-    #     # Unzip tables
-    #     print("Unzipping: {}".format(table_name))
-    #     unzip(inputdir, table_name)
 
     # uspc_patents_url = 'https://bulkdata.uspto.gov/data/patent/classification/mcfpat.zip'
     # uspc_applications_url = 'https://bulkdata.uspto.gov/data/patent/classification/mcfappl.zip'
     # download(uspc_patents_url, inputdir, 'uspc_patents.zip')
     # download(uspc_applications_url, inputdir, 'uspc_applications.zip')
 
-    parse_and_write_uspc(inputdir, outputdir)
+    parse_and_write_uspc(inputdir, outputdir)    
