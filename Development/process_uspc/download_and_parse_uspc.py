97e8f694732bc2ab3682e23d2951a7851c9d8b01
sarahkelley <sarahkelley1759@gmail.com>
2018-09-25 22:30:10 +0000

    Adding USPC code

---
+++ Development/process_uspc/download_and_parse_uspc.py
@@ -0,0 +1,231 @@
+import re
+import os
+from zipfile import ZipFile
+import sys
+sys.path.append('{}/{}'.format(os.getcwd(), 'Development'))
+from helpers import general_helpers
+
+
+def parse_and_write_uspc(inputdir, outputdir):
+    """ Parse and write USPC info to CSV tables """
+    uspc_applications = parse_uspc_applications(inputdir,
+                                                'uspc_applications.zip')
+    general_helpers.write_csv(uspc_applications, outputdir,
+              'USPC_application_classes_data.csv')
+
+    uspc_patents = parse_uspc_patents(inputdir, 'uspc_patents.zip')
+    general_helpers.write_csv(uspc_patents, outputdir,
+              'USPC_patent_classes_data.csv')
+
+
+def parse_uspc_applications(inputdir, zip_filename):
+    """
+    Parse USPC Application information from the USPTO MCF Application zip file
+    Original Data:
+        US20180027683A1361724000S
+        US20180027707A1361752000S
+        US20180042153P1PLT258000S
+        US20180084553A1D14240000S
+        US20180092099A1D14240000S
+    Parsed Rows:
+        Patent Number       Main Class      Subclass    Classification Type
+        -------------------------------------------------------------------
+        2018/20180027677,   29,             525.1,      2
+        2018/20180027677,   312,            265.5,      3
+        2018/20180027683,   29,             428,        1
+        2018/20180027683,   211,            26,         2
+        2018/20180027683,   312,            223.2,      3
+    """
+    global found_patents
+    found_patents = {}
+
+    zip = ZipFile(os.path.join(inputdir, zip_filename), 'r')
+
+    # The zip file should contain a single text file like 'mcfappl[\d]+.zip'
+    number_of_files_in_zip = len(zip.namelist())
+    name_of_first_file_in_zip = zip.namelist()[0]
+    assert(number_of_files_in_zip == 1 and
+           re.search('mcfappl[\d]+\.txt$', name_of_first_file_in_zip))
+
+    rows = []
+    with zip.open(name_of_first_file_in_zip) as f:
+        for classification in f:
+            # TODO: Check with the team that this is correct
+            row = parse_uspc_application(classification.decode('utf-8'))
+            rows.append(row)
+
+    return rows
+
+
+def parse_uspc_application(row):
+    """
+    Parse USPC Application information from a USPTO MCF Application entry
+    Original Data:
+        US20010000001A1520001000P
+    Parsed Row:
+        2001/20010000001,520,1,0
+    """
+    # Patent Number
+    #print(row)
+    patent_number = row[2:6] + '/' + row[2:13]
+
+    # Main Class
+    main_class = re.sub('^0+', '', row[15:18])
+
+    # Subclass
+    subclass = parse_subclass(row[18:-2])
+
+    # Classification Type
+    order = parse_order(class_type_char=row[-2],
+                        patent_number=patent_number, primary_type_char='P')
+    return [patent_number, main_class, subclass, order]
+
+
+def parse_subclass(subclass):
+    """ Parse subclass informatinon from the relevant subclass string """
+    left = subclass[:3]
+    right = subclass[3:]
+
+    # TODO: Reorganize logic to be more readable
+    if right == '000':
+        subclass = left.lstrip('0')
+    else:
+        if right.isdigit():
+            if left.isalpha():
+                subclass = left.lstrip('0') + right.lstrip('0')
+            else:
+                if left[0].isalpha():
+                    subclass = left.replace('0', '') + '.' + right
+                else:
+                    subclass = left.lstrip('0') + '.' + right.replace('0', '')
+        else:
+            if len(right.replace('0', '')) > 1:
+                subclass = left.lstrip('0') + '.' + right.replace('0', '')
+            else:
+                subclass = left.lstrip('0') + right.replace('0', '')
+
+    return subclass
+
+
+def parse_uspc_patents(inputdir, zip_filename):
+    """
+    Parse USPC Patent information from the USPTO MCF Patent zip file
+    Original Data:
+        0000001295004000O
+        0000001016100000X
+        0000002057058490O
+        0000003142042000O
+        0000003142048000X
+        0000003144012000X
+    Parsed Rows:
+        Patent Number       Main Class      Subclass    Classification Type
+        -------------------------------------------------------------------
+        2018/20180027677,   29,             525.1,      2
+        2018/20180027677,   312,            265.5,      3
+        2018/20180027683,   29,             428,        1
+        2018/20180027683,   211,            26,         2
+        2018/20180027683,   312,            223.2,      3
+    """
+    global found_patents
+    found_patents = {}
+    rows = []
+
+    zip = ZipFile(os.path.join(inputdir, zip_filename), 'r')
+
+    # The zip file should contain a single text file like 'mcfappl[\d]+.zip'
+    number_of_files_in_zip = len(zip.namelist())
+    name_of_first_file_in_zip = zip.namelist()[0]
+    assert(number_of_files_in_zip == 1 and
+           re.search('mcfpat[\d]+\.txt$', name_of_first_file_in_zip))
+
+    with zip.open(name_of_first_file_in_zip) as f:
+        for classification in f:
+            row = parse_uspc_patent(classification.decode('utf-8'))
+            rows.append(row)
+
+    return rows
+
+
+def parse_uspc_patent(row):
+    """
+    Parse USPC Patent information from a USPTO MCF Patent entry
+    Original Data:
+        0000001295004000O
+    Parsed Row:
+        0000001,295,4,0
+    """
+    # Patent Number
+    patent_number = row[:7]
+
+    # Main Class
+    main_class = re.sub('^0+', '', row[7:10])
+
+    # Subclass
+    subclass = parse_subclass(row[10:-2])
+
+    # Classification Type
+    order = parse_order(class_type_char=row[-2],
+                        patent_number=patent_number, primary_type_char='O')
+
+    return [patent_number, main_class, subclass, order]
+
+
+def parse_order(class_type_char, patent_number, primary_type_char):
+    """ Parse classification order from row, using the global counter
+    of found patents to increment order each time a patent is seen again. For
+    example, a Primary and three Secondary USPC Classifications will have
+    orders 0, 1, 2, and 3 respectively. (The Primary will always have order 0,
+    and the Secondary will have orders incremented by the order of appearance)
+    class_type_char: the USPC classification character
+                        that indicates primary or secondary
+    patent_number: 7 digit patent number parsed from the USPC classification
+    primary_type_char: The character that represents primary
+                        (typically 'O' for patents or 'P' for applications)
+    """
+
+    if class_type_char == primary_type_char:
+        # Primary Patents -> class_type = 0
+        class_type = str(0)
+    else:
+        # Secondary Patents -> class_type = 1, 2, 3, ... (incrementing)
+        if patent_number in found_patents:
+            found_patents[patent_number] += 1
+        else:
+            found_patents[patent_number] = 1
+        class_type = str(found_patents[patent_number])
+
+    return class_type
+
+if __name__ == '__main__':
+
+    import configparser
+    config = configparser.ConfigParser()
+    config.read('Development/config.ini')
+
+    inputdir = '{}/uspc_input'.format(config['FOLDERS']['WORKING_FOLDER'])
+    outputdir = '{}/uspc_output'.format(config['FOLDERS']['WORKING_FOLDER'])
+
+    print("Parsing and writing USPC Classification tables... \n")
+    print("INPUT DIRECTORY: {}".format(inputdir))
+    print("OUTPUT DIRECTORY: {}".format(outputdir))
+
+    if not os.path.exists(inputdir):
+         os.makedirs(inputdir)
+    # Download USPC Patent Classifications
+    uspc_patents_url = 'https://bulkdata.uspto.gov/data/patent' \
+                       '/classification/mcfpat.zip'
+    uspc_patents_filepath = os.path.join(inputdir, 'uspc_patents.zip')
+    #general_helpers.download(uspc_patents_url, uspc_patents_filepath)
+
+    # Download USPC Application Classifications
+    uspc_applications_url = 'https://bulkdata.uspto.gov/data/patent' \
+                            '/classification/mcfappl.zip'
+    uspc_applications_filepath = os.path.join(inputdir,
+                                              'uspc_applications.zip')
+    #general_helpers.download(uspc_applications_url, uspc_applications_filepath)
+    print('downloaded')
+    if not os.path.exists(outputdir):
+        os.makedirs(outputdir)
+    
+    # Parse USPC Classifications and write to CSV
+    parse_and_write_uspc(inputdir, outputdir)
---
+++ Development/process_uspc/upload_uspc.py
@@ -0,0 +1,141 @@
+import sys
+import os
+import re
+import csv
+sys.path.append('{}/{}'.format(os.getcwd(), 'Development'))
+from helpers import general_helpers
+import pandas as pd
+import multiprocessing
+
+def write_uspc_current(in_file, out_file, error_log, patent_dict, patent_set):
+    #Create USPC_current table off full master classification list
+    uspc_data= csv.reader(open(in_file,'r'), delimiter = ',')
+    errorlog = open(error_log,'w')
+    uspc_current_file = csv.writer(open(out_file, 'w'), delimiter = '\t')
+    for_csv_list =[]
+    counter = 0
+    for row in uspc_data:
+        counter +=1
+        if counter%100000==0:
+            print(counter)
+        if str(row[0]) in patent_set:
+            towrite = [re.sub('"',"'",item) for item in row]
+            towrite.insert(0,id_generator())
+            towrite[1] = patent_dict[row[0]]
+            for t in range(len(towrite)):
+                try:
+                    gg = int(towrite[t])
+                    towrite[t] = str(int(towrite[t]))
+                except:
+                    pass
+            towrite[3] = towrite[2]+'/'+re.sub('^0+','',towrite[3])
+            #change only for patents from after 2015-05-31, based on sequential numbering
+            if is_after_2015(towrite[1]):
+                if towrite[3] == '1/1':
+                    towrite[3] = "No longer published"
+                    towrite[2] = "No longer published"
+            uspc_current_file.writerow(towrite)
+
+def is_after_2015(patent_id):
+    '''determine if a patent is from after 2015 based on sequential numbering, cutoffs determined empirically '''
+    try:
+        patent_id = int(patent_id)
+        return True if patent_id >=9043948  else False
+    except:
+        first_char = patent_id[0]
+        if first_char == 'D':
+            return True if int(patent_id[1:])>=730616 else False
+        elif first_char =='P':
+            return True if int(patent_id[2:])>=25608 else False
+        elif first_char=='R':
+            return True if int(patent_id[2:])>=45533 else False
+        elif first_char=='T' or first_char=='H':
+            return False #no patents starting the T or H are past the cut off date
+        else:
+            print(patent_id)
+            return False
+
+def upload_uspc_current(db_con, uspc_current_loc):
+    uspc_current_files = [f for file in os.listdir(uspc_current_loc) if f.startswith('out')]
+    for outfile in uspc_current_files:
+        data = pd.read_csv('{}/{}'.format(uspc_current_loc,outfile),delimiter = '\t', encoding ='utf-8')
+    data.to_sql(uspc_current, db_con, if_exists = 'append', index=False)
+
+
+def find_missed_patents(db_con, patent_set, uspc_output):
+    patents_in_uspc = db_con.execute('select patent_id from {}.uspc_current'.format(new_db))
+    current_exists = set()
+    for patent in patents_in_uspc:
+        current_exists.add(patent['patent_id'])
+
+    missed_patents = list(patent_set - current_exists)
+
+    if len(missed_patents) > 0:
+        main = db_con.execute('select id from mainclass_current')
+        mainclass = set([item['id'] for item in main])
+        sub = db_con.execute('select id from subclass_current')
+        subclass = set([item['id'] for item in sub])
+
+        to_add_main = []
+        to_add_sub = []
+        to_add_uspc_current = []
+
+        count =0
+        for patent in missed_patents:
+            db_con.execute('select * from uspc where patent_id ="'+str(patent)+'"')
+            datum = cursor.fetchall()
+            count +=1
+            if count%1000 ==0:
+                print(str(count))
+            for d in datum:
+                if not d['mainclass_id'] in mainclass:
+                    to_add_main.append(d['mainclass_id'])
+                if not d['subclass_id'] in subclass:
+                    to_add_sub.append(d['subclass_id'])
+                d['sequence'] = int(d['sequence'])
+                to_add_uspc_current.append(d['uuid'], d['patent_id'], d['mainclass_id'], d['subclass_id'], d['sequence']) 
+    print("got final data for update")
+    data = pd.DataFrame(to_add_all)
+    data.columns = ['uuid', 'patent_id', 'mainclass_id', 'subclass_id', 'sequence']
+    data.to_sql(con=db_con, name='temp_uspc_current_to_insert', index = False, if_exists='replace')
+    print('uploaded data, inserting')
+    db_con.execute('insert into uspc_current select * from temp_uspc_current_to_insert;')
+    print("done")
+
+
+if __name__ == '__main__':
+    import configparser
+    config = configparser.ConfigParser()
+    config.read('Development/config.ini')
+
+    db_con = general_helpers.connect_to_db(config['DATABASE']['HOST'], config['DATABASE']['USERNAME'], 
+                                            config['DATABASE']['PASSWORD'], config['DATABASE']['NEW_DB'])
+
+    patent_set, patent_dict = general_helpers.get_patent_ids(db_con, config['DATABASE']['NEW_DB'])
+
+    uspc_folder = '{}/uspc_output'.format(config['FOLDERS']['WORKING_FOLDER'])
+
+    os.system('split -a 1 -n 7 {0}/USPC_patent_classes_data.csv {0}/uspc_patent_pieces_'.format(uspc_folder))
+
+    in_files = ['{0}/uspc_patent_pieces_{1}'.format(uspc_folder, item) for item in  ['a', 'b', 'c', 'd', 'e', 'f', 'g']]
+    out_files = ['{0}/out_file_{1}.csv'.format(uspc_folder, item) for item in  ['a', 'b', 'c', 'd', 'e', 'f', 'g']]
+    error_log = ['{0}/error_log_{1}'.format(uspc_folder, item) for item in  ['a', 'b', 'c', 'd', 'e', 'f', 'g']]
+
+    pat_dicts = [patent_dict for item in in_files]
+    pat_sets = [patent_set for item in in_files]
+    files = zip(in_files, out_files, error_log, pat_dicts, pat_sets)
+
+
+    desired_processes = 7# ussually num cpu - 1
+    jobs = []
+    for f in files:
+        jobs.append(multiprocessing.Process(target = write_uspc_current, args=(f)))
+
+    for segment in general_helpers.chunks(jobs, desired_processes):
+        for job in segment:
+            print(segment)
+            job.start()
+
+    upload_uspc_current(db_con, uspc_folder)
+
+    find_missed_patents(db_con, patent_set, '{}/uspc_current.csv'.format(folder))
