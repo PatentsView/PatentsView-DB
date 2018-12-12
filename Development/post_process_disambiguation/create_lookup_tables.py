5c62e8007b6054d26532b013a87651a253a05b9a
sarahkelley <sarahkelley1759@gmail.com>
2018-12-12 20:28:31 +0000

    Fixing location post processing

--- Development/post_process_disambiguation/create_lookup_tables.py
+++ Development/post_process_disambiguation/create_lookup_tables.py
@@ -19,20 +19,25 @@ config.read('/usr/local/airflow/PatentsView-DB/Development/config.ini')
 
 db_con = general_helpers.connect_to_db(config['DATABASE']['HOST'], config['DATABASE']['USERNAME'], config['DATABASE']['PASSWORD'], config['DATABASE']['NEW_DB'])
 
-disambiguated_folder = "{}/disambig_out".format(config['FOLDERS']['WORKING_FOLDER'])
-
+'''
 db_con.execute('insert into patent_inventor select patent_id, inventor_id from rawinventor')
 print('patent_inventor')
 db_con.execute('insert into patent_assignee select patent_id, assignee_id from rawassignee')
 print('patent_assignee')
 db_con.execute('insert into patent_lawyer select patent_id, lawyer_id from rawlawyer')
 print('patent_lawyer')
-db_con.execute('insert into location_assignee select location_id_transformed, assignee_id from rawassignee')
+'''
+db_con.execute("create table temp_assignee_loc as select assignee_id, rawlocation_id, location_id_transformed as location_id from rawassignee r left join rawlocation l on r.rawlocation_id = l.id;")
+print('made locaiton_assignee_temp')
+db_con.execute("insert into location_assignee (select max(location_id) as location_id, a.assignee_id from (select assignee_id, max(location_id_count) as max_count from (select assignee_id, location_id, count(location_id) as location_id_count from temp_assignee_loc group by assignee_id, location_id) as m group by assignee_id) as a left join (select assignee_id, location_id, count(location_id) as location_id_count from temp_assignee_loc group by assignee_id, location_id) as b on a.assignee_id =b.assignee_id and a.max_count=b.location_id_count group by a.assignee_id);")
+
 print('location_assignee')
 #I don't understand why location assignee and location inventor have different types of location id
-db_con.execute('insert into location_inventor select location_id, inventor_id from rawinventor')
+db_con.execute("create table temp_inventor_loc as select inventor_id, location_id from rawinventor r left join rawlocation l on r.rawlocation_id = l.id;")
+print('done inventor temp')
+db_con.execute('insert into location_inventor (select max(location_id) as location_id, a.inventor_id from (select inventor_id, max(location_id_count) as max_count from (select inventor_id, location_id, count(location_id) as location_id_count from temp_inventor_loc where location_id !='' and location_id is not null group by inventor_id, location_id) as m group by inventor_id) as a left join (select inventor_id, location_id, count(location_id) as location_id_count from temp_inventor_loc where location_id !='' and location_id is not null group by inventor_id, location_id) as b on a.inventor_id =b.inventor_id and a.max_count=b.location_id_count group by a.inventor_id);')
+
 print('location_inventor')
- 
 
 
 
--- Development/post_process_disambiguation/post_process_location.py
+++ Development/post_process_disambiguation/post_process_location.py
@@ -15,7 +15,7 @@ from helpers import general_helpers
 def make_lookup(disambiguated_folder):
     inp = csv.reader(open( "{}/location_disambiguation.tsv".format(disambiguated_folder),'r'),delimiter='\t')
     print('read in')
-    lat_name_lookup = {} #place to lat/long/id lookup
+    name_id_lookup = {} #place to lat/long/id lookup
     undisambiguated =set() #place for locations without a lat/long
     lookup = {} #raw location id to disambiguated id
     #for some reason column 1 and 2 both have disambiguated place names
@@ -26,35 +26,41 @@ def make_lookup(disambiguated_folder):
         if counter%500000 == 0:
            print(counter)
         #TODO: need to check if this is the correct structure 
-        clean_loc = tuple([None if i =='NULL' else i for i in row[1].split('|')])
-        if row[3] != 'NULL':#if there is a lat-long pair
-            lat_long = "{0}|{1}".format(np.round(float(row[3]),4), np.round(float(row[4]),4))
-            lat_name_lookup[lat_long] = {'place': clean_loc, 'id':general_helpers.id_generator(12)}
-        else: #for the place without a lat long lookup
-            undisambiguated.add(row[0])
+        if not row[1] in name_id_lookup.keys():
+            clean_loc = tuple([None if i =='NULL' else i for i in row[1].split('|')])
+            if len(row) < 5: #some of the lat long pairs are ented as a pipe separated pair in the 4th column
+                lat_long = row[3].split('|')
+                row[3] = lat_long[0]
+                row.append(lat_long[1])
+            if row[3] != 'NULL':#if there is a lat-long pair
+                lat_long = "{0}|{1}".format(np.round(float(row[3]),4), np.round(float(row[4]),4))
+            else:
+                lat_long = '|'
+            name_id_lookup[row[1]] = {'place': clean_loc, 'lat_long':lat_long, 'id':general_helpers.id_generator(12)}
     print('first loop')
-    print('length of undisambiguated: {}'.format(len(undisambiguated)))
     #have to read the file in again to go back over it
     inp = csv.reader(open("{}/location_disambiguation.tsv".format(disambiguated_folder),'r'),delimiter='\t')
     #second round to produced looked up info
     for e, row in enumerate(inp):    
-        if row[3] != 'NULL':
-            lat_long = "{0}|{1}".format(np.round(float(row[3]),4), np.round(float(row[4]),4))
-            lookup[row[0]] = {'id': lat_name_lookup[lat_long]['id'], 'lat_long':lat_long}
+         lookup[row[0]] = row[1]
     print('second loop')
-    return lat_name_lookup, lookup, undisambiguated
+    return name_id_lookup, lookup
 
-def upload_location(db_con, lat_name_lookup, disambiguated_folder):
+def upload_location(db_con, name_id_lookup, disambiguated_folder):
     location = []          
-    for k, v in lat_name_lookup.items():
-        location.append([v['id'], v['place'][0],v['place'][1],v['place'][2],k.split('|')[0], k.split('|')[1], None, None, None])
+    for k, v in name_id_lookup.items():
+        try:
+            location.append([v['id'], v['place'][0],v['place'][1],v['place'][2],v['lat_long'].split('|')[0], v['lat_long'].split('|')[1], None, None, None])
+        except:
+            print('key: ', k)
+            print('value: ', v)
     location_df = pd.DataFrame(location)
     location_df.columns = ['id', 'city', 'state', 'country', 'latitude', 'longitude', 'county', 'state_fips', 'county_fips']
     location_df.to_sql(con=db_con, name = 'location', if_exists = 'append', index = False)
     location_df.to_csv('{}/location.csv'.format(disambiguated_folder))
     
 
-def process_rawlocation(db_con, lookup, lat_name_lookup, undisambiguated, disambiguated_folder):
+def process_rawlocation(db_con, lookup, name_id_lookup, disambiguated_folder):
    
     db_con.execute('alter table rawlocation rename temp_rawlocation_backup')
     print('getting data for rawloc')
@@ -69,24 +75,14 @@ def process_rawlocation(db_con, lookup, lat_name_lookup, undisambiguated, disamb
         counter +=1
         if counter%500000==0:
             print(str(counter))
-        if i['id'] in lookup.keys(): 
-            loc_id = lookup[i['id']]['id']
-            lat_long =lookup[i['id']]['lat_long']
+        if i['id'] in lookup.keys():
+            cannonical_name = lookup[i['id']]
+            lat_long = name_id_lookup[cannonical_name]['lat_long']
+            loc_id = name_id_lookup[cannonical_name]['id']
+            updated.writerow([i['id'],loc_id, i['city'], i['state'], i['country'], i['country_transformed'], lat_long])
         else:
-            loc_id = None
-            lat_long = None
-            #need to update the rest to null and count them
-            if i[0] in undisambiguated:
-                total_undisambiguated +=1 
-            else:
-                total_missed.append(i[0])
-        updated.writerow([i['id'],loc_id, i['city'], i['state'], i['country'], i['country_transformed'], lat_long])
-    try:
-        print(len(total_missed))
-        print(totally_undisambiguated)
-    except:
-        print('no')
-    print('now reading in fo upload')
+            updated.writerow([i['id'],'', i['city'], i['state'], i['country'
+], i['country_transformed'],''])
 
 def upload_rawloc(db_con, disambiguated_folder):
     raw_data = pd.read_csv(disambiguated_folder + "/rawlocation_updated.csv", delimiter = '\t')
@@ -102,13 +98,14 @@ if __name__ == '__main__':
     db_con = general_helpers.connect_to_db(config['DATABASE']['HOST'], config['DATABASE']['USERNAME'], config['DATABASE']['PASSWORD'], config['DATABASE']['NEW_DB'])
     
     disambiguated_folder = "{}/disambig_out".format(config['FOLDERS']['WORKING_FOLDER'])
+    '''
     print('here!')
     print('data inserted')
-    lat_name_lookup, lookup, undisambiguated = make_lookup(disambiguated_folder)
+    name_id_lookup, lookup = make_lookup(disambiguated_folder)
     print('made lookup')
-    upload_location(db_con, lat_name_lookup, disambiguated_folder)
+    upload_location(db_con, name_id_lookup, disambiguated_folder)
     print('done locupload ')
-    process_rawlocation(db_con, lookup, lat_name_lookup,undisambiguated, disambiguated_folder)
+    process_rawlocation(db_con, lookup, name_id_lookup, disambiguated_folder)
     print('done process')
+    '''
     upload_rawloc(db_con, disambiguated_folder)
-    
