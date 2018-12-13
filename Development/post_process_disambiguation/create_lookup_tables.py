5adc2429a44ebb43ecf7109d261b9d11ecd0339d
sarahkelley <sarahkelley1759@gmail.com>
2018-12-13 12:38:25 -0500

    uncommenting create lookup

--- Development/post_process_disambiguation/create_lookup_tables.py
+++ Development/post_process_disambiguation/create_lookup_tables.py
@@ -19,7 +19,7 @@ config.read('/usr/local/airflow/PatentsView-DB/Development/config.ini')
 
 db_con = general_helpers.connect_to_db(config['DATABASE']['HOST'], config['DATABASE']['USERNAME'], config['DATABASE']['PASSWORD'], config['DATABASE']['NEW_DB'])
 
-'''
+
 db_con.execute('insert into patent_inventor select patent_id, inventor_id from rawinventor')
 print('patent_inventor')
 db_con.execute('insert into patent_assignee select patent_id, assignee_id from rawassignee')
@@ -33,7 +33,7 @@ print('location_assignee')
 #I don't understand why location assignee and location inventor have different types of location id
 db_con.execute("create table temp_inventor_loc as select inventor_id, location_id from rawinventor r left join rawlocation l on r.rawlocation_id = l.id;")
 print('done inventor temp')
-'''
+
 db_con.execute("insert into location_inventor (select max(location_id) as location_id, a.inventor_id from (select inventor_id, max(location_id_count) as max_count from (select inventor_id, location_id, count(location_id) as location_id_count from temp_inventor_loc where location_id !='' and location_id is not null group by inventor_id, location_id) as m group by inventor_id) as a left join (select inventor_id, location_id, count(location_id) as location_id_count from temp_inventor_loc where location_id !='' and location_id is not null group by inventor_id, location_id) as b on a.inventor_id =b.inventor_id and a.max_count=b.location_id_count group by a.inventor_id);")
 print('location_inventor')
 
