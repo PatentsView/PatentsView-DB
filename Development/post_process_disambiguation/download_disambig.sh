381f1f1269f0b61ca4cfb85a73242eeeec495a26
sarahkelley <sarahkelley1759@gmail.com>
2018-12-13 12:37:36 -0500

    remove old download file

--- Development/post_process_disambiguation/download_disambig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-scp -i "PatentsView-DB/Development/PV_Apache_Solr.pem centos@ec2-52-21-62-204.compute-1.amazonaws.com:/data/inventor-disambiguation-internal/data/multi-canopy-output/clean_inventor_results.txt inventor_disambiguation.tsv
-
-scp -i "PatentsView-DB/Development/PV_Apache_Solr.pem" centos@ec2-52-21-62-204.compute-1.amazonaws.com:/data/inventor-disambiguation-internal/exp_out/assignee/disambiguation.post_processed.tsv assignee_disambiguation.tsv
-
-scp -i "PatentsView-DB/Development/PV_Apache_Solr.pem" centos@ec2-52-21-62-204.compute-1.amazonaws.com:/data/inventor-disambiguation-internal/exp_out/location_all.tsv .
