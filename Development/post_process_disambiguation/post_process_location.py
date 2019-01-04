98160b8defecc800deaf402c27c5c6a4406cdc35
sarahkelley <skelley@air.org>
2019-01-04 13:03:48 -0500

    Fixing same lat-long pair with different cannoncial name problem

--- Development/post_process_disambiguation/post_process_location.py
+++ Development/post_process_disambiguation/post_process_location.py
@@ -20,47 +20,41 @@ def make_lookup(disambiguated_folder):
     lookup = {} #raw location id to disambiguated id
     #for some reason column 1 and 2 both have disambiguated place names
     #create the latitude/longitude to id mappings
-    counter = 0
+    id_lat_long_lookup = {}
+    lat_long_name_count = defaultdict(lambda : defaultdict(lambda: 0))
     for row in inp:
-        counter +=1 
-        if counter%500000 == 0:
-           print(counter)
-        #TODO: need to check if this is the correct structure 
-        if not row[1] in name_id_lookup.keys():
-            clean_loc = tuple([None if i =='NULL' else i for i in row[1].split('|')])
-            if len(row) < 5: #some of the lat long pairs are ented as a pipe separated pair in the 4th column
-                lat_long = row[3].split('|')
-                row[3] = lat_long[0]
-                row.append(lat_long[1])
-            if row[3] != 'NULL':#if there is a lat-long pair
-                lat_long = "{0}|{1}".format(np.round(float(row[3]),4), np.round(float(row[4]),4))
-            else:
-                lat_long = '|'
-            name_id_lookup[row[1]] = {'place': clean_loc, 'lat_long':lat_long, 'id':general_helpers.id_generator(12)}
+        clean_loc = tuple([None if i =='NULL' else i for i in row[1].split('|')])
+        if len(row) < 5: #some of the lat long pairs are ented as a pipe separated pair in the 4th column
+            lat_long = row[3].split('|')
+            row[3] = lat_long[0]
+            row.append(lat_long[1])
+        if row[3] != 'NULL':#if there is a lat-long pair
+            lat_long = "{0}|{1}".format(np.round(float(row[3]),4), np.round(float(row[4]),4))
+        else:
+            lat_long = '|' #this handles the ~13,000 undisambiguated
+        lat_long_name_count[lat_long][clean_loc] +=1  
+        id_name_lookup[row[0]] = lat_long
     print('first loop')
-    #have to read the file in again to go back over it
-    inp = csv.reader(open("{}/location_disambiguation.tsv".format(disambiguated_folder),'r'),delimiter='\t')
-    #second round to produced looked up info
-    for e, row in enumerate(inp):    
-         lookup[row[0]] = row[1]
-    print('second loop')
-    return name_id_lookup, lookup
 
-def upload_location(db_con, name_id_lookup, disambiguated_folder):
+    lat_long_cannonical_name = {}
+    for lat_long, name_list in lat_long_name_count.items():
+        name = sorted([(name, count) for name, count in name_list.items()], key = lambda x: x[1])[-1]
+        lat_long_cannonical_name[lat_long] = {'place': name, 'id': general_helpers.id_generator(12)}
+
+    print('second loop')
+    return id_lat_long_lookup, lat_long_cannonical_name
+
+def upload_location(db_con, lat_long_cannonical_name, disambiguated_folder):
     location = []          
-    for k, v in name_id_lookup.items():
-        try:
-            location.append([v['id'], v['place'][0],v['place'][1],v['place'][2],v['lat_long'].split('|')[0], v['lat_long'].split('|')[1], None, None, None])
-        except:
-            print('key: ', k)
-            print('value: ', v)
+    for lat_long, v in lat_long_cannonical_name.items():
+        location.append([v['id'], v['place'][0],v['place'][1],v['place'][2],lat_long.split('|')[0], lat_long.split('|')[1], None, None, None])
     location_df = pd.DataFrame(location)
     location_df.columns = ['id', 'city', 'state', 'country', 'latitude', 'longitude', 'county', 'state_fips', 'county_fips']
     location_df.to_sql(con=db_con, name = 'location', if_exists = 'append', index = False)
     location_df.to_csv('{}/location.csv'.format(disambiguated_folder))
     
 
-def process_rawlocation(db_con, lookup, name_id_lookup, disambiguated_folder):
+def process_rawlocation(db_con, lat_long_cannonical_name, id_lat_long_lookup, disambiguated_folder):
    
     db_con.execute('alter table rawlocation rename temp_rawlocation_backup')
     print('getting data for rawloc')
@@ -75,14 +69,12 @@ def process_rawlocation(db_con, lookup, name_id_lookup, disambiguated_folder):
         counter +=1
         if counter%500000==0:
             print(str(counter))
-        if i['id'] in lookup.keys():
-            cannonical_name = lookup[i['id']]
-            lat_long = name_id_lookup[cannonical_name]['lat_long']
-            loc_id = name_id_lookup[cannonical_name]['id']
+        if i['id'] in id_lat_long_lookup.keys():
+            lat_long = id_lat_long_lookup[i['id']]
+            loc_id = lat_long_cannonical_name[lat_long]['id']
             updated.writerow([i['id'],loc_id, i['city'], i['state'], i['country'], i['country_transformed'], lat_long])
         else:
-            updated.writerow([i['id'],'', i['city'], i['state'], i['country'
-], i['country_transformed'],''])
+            updated.writerow([i['id'],'', i['city'], i['state'], i['country'], i['country_transformed'],''])
 
 def upload_rawloc(db_con, disambiguated_folder):
     raw_data = pd.read_csv(disambiguated_folder + "/rawlocation_updated.csv", delimiter = '\t')
@@ -98,14 +90,14 @@ if __name__ == '__main__':
     db_con = general_helpers.connect_to_db(config['DATABASE']['HOST'], config['DATABASE']['USERNAME'], config['DATABASE']['PASSWORD'], config['DATABASE']['NEW_DB'])
     
     disambiguated_folder = "{}/disambig_out".format(config['FOLDERS']['WORKING_FOLDER'])
-    '''
     print('here!')
     print('data inserted')
-    name_id_lookup, lookup = make_lookup(disambiguated_folder)
+    id_lat_long_lookup, lat_long_cannonical_name = make_lookup(disambiguated_folder)
     print('made lookup')
-    upload_location(db_con, name_id_lookup, disambiguated_folder)
+    upload_location(db_con, lat_long_cannonical_name, disambiguated_folder)
     print('done locupload ')
-    process_rawlocation(db_con, lookup, name_id_lookup, disambiguated_folder)
+    process_rawlocation(db_con, lat_long_cannonical_name, d_lat_long_lookup, disambiguated_folder)
     print('done process')
-    '''
-    upload_rawloc(db_con, disambiguated_folder)
+    #upload_rawloc(db_con, disambiguated_folder)
+
+
