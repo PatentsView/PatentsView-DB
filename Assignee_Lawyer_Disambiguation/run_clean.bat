4d8b16f31ac863ef6034e23fa6174d5e1fcc4858
kjallawaram <kjallawaram@air.org>
2019-01-24 13:58:37 -0500

    Code clean-up

--- Assignee_Lawyer_Disambiguation/run_clean.bat
+++ Assignee_Lawyer_Disambiguation/run_clean.bat
@@ -8,6 +8,3 @@ python lib/assignee_disambiguation.py
 REM TODO: fixup lawyer disambiguation
 REM echo 'Running lawyer disambiguation'
 REM python lib/lawyer_disambiguation.py grant
-
-REM echo 'Running geo disambiguation'
-REM python lib/geoalchemy.py
