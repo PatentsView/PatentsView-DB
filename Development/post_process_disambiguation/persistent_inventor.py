2737b5abe0a98bbe25330004912f083b7735703e
sarahkelley <skelley@air.org>
2019-02-10 16:26:24 +0000

    Adding inventor gender lookup

--- Development/post_process_disambiguation/persistent_inventor.py
+++ Development/post_process_disambiguation/persistent_inventor.py
@@ -3,13 +3,13 @@ import os
 import csv
 import sys
 import pandas as pd
-sys.path.append('/usr/local/airflow/PatentsView-DB/Development')
+sys.path.append('/project/Development')
 sys.path.append('{}/{}'.format(os.getcwd(), 'Development'))
 from helpers import general_helpers
 
 import configparser
 config = configparser.ConfigParser()
-config.read('/usr/local/airflow/PatentsView-DB/Development/config.ini')
+config.read('/project/Development/config.ini')
 disambig_folder = '{}/{}'.format(config['FOLDERS']['WORKING_FOLDER'],'disambig_out')
 old_db = config['DATABASE']['OLD_DB']
 new_db = config['DATABASE']['NEW_DB']
@@ -44,16 +44,38 @@ for inv in new_data:
     else:
         outfile.writerow([inv['uuid'], '', inv['inventor_id']] + blanks)
 
-#db_con.execute('alter table persistent_inventor_disambig add disamb_inventor_id_{} varchar(24)'.format(new_update_date))
+db_con.execute('alter table persistent_inventor_disambig add disamb_inventor_id_{} varchar(24)'.format(new_update_date))
 data = pd.read_csv('{}/inventor_persistent.tsv'.format(disambig_folder), encoding = 'utf-8', delimiter = '\t')
 data.to_sql(con=db_con, name = 'persistent_inventor_disambig', index = False, if_exists='append') #append keeps the indexes
-db_con.execute('create index {0}_ix on persistent_inventor_disambig ({0});'.format(new_id_col)
+db_con.execute('create index {0}_ix on persistent_inventor_disambig ({0});'.format(new_id_col))
 
-#create the gender lookup table
-query = '''create table gender_lookup as
-select p.{} as inventor_id, t.dumale as male from
-temp_inventor_gender_base_table t left join persistent_inventor_disambig p 
-on t.id = p.disamb_inventor_id_20170808;'''.format(new_id_col).replace('\n', ' ')
-db_con.execute(query)
-db_con.close()
+new_id_col = 'disamb_inventor_id_{}'.format(new_db[-8:])
+
+#create inventor gender lookup
+new_id_col = 'disamb_inventor_id_{}'.format(new_db[-8:])
+new_to_old = con.execute('select disamb_inventor_id_20170808, {} from {}.persistent_inventor_disambig'.format(new_id_col, new_db))
+
+ids_new_to_old = {}
+for row in new_to_old:
+     ids_new_to_old[row[new_id_col]] = row['disamb_inventor_id_20170808']
+query = 'select id, dumale from {}.temp_inventor_gender_base_table'.format(new_db)
+
+gender = con.execute(query)
+id_to_gender = {}
+for row in gender:
+    id_to_gender[row['id']] = row['dumale']
+
+results = []
+for new_id, old_id in ids_new_to_old.items(): 
+    if old_id is not None:
+        results.append((new_id, id_to_gender[old_id]))
+    else:
+        results.append((new_id, None))
+gender_df = pd.DataFrame(results)
+gender_df.columns = ['inventor_id', 'male']
+
+gender_df.to_sql(con=con, name = 'inventor_gender', index = False, if_exists='append')
+
+               
+
 
