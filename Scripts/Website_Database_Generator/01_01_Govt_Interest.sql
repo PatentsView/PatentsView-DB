7e40ca839f9caba4fa319f03e1c3d1971550d650
sarahkelley <sarahkelley1759@gmail.com>
2019-02-20 01:08:26 -0500

    Revert "Revert "Reporting db optimization""

---
+++ Scripts/Website_Database_Generator/01_01_Govt_Interest.sql
@@ -0,0 +1,62 @@
+
+# BEGIN Government interest tables 
+
+###############################################################################################################################
+drop table if exists `{{params.reporting_database}}`.`government_interest`;
+CREATE TABLE `{{params.reporting_database}}`.`government_interest` (
+   `patent_id` varchar(255) NOT NULL,
+   `gi_statement` text,
+   PRIMARY KEY (`patent_id`)
+ ) ENGINE=InnoDB;
+
+drop table if exists `{{params.reporting_database}}`.`government_organization`;
+CREATE TABLE IF NOT EXISTS `{{params.reporting_database}}`.`government_organization` (
+   `organization_id` int(11) NOT NULL AUTO_INCREMENT,
+   `name` varchar(255) DEFAULT NULL,
+   `level_one` varchar(255) DEFAULT NULL,
+   `level_two` varchar(255) DEFAULT NULL,
+   `level_three` varchar(255) DEFAULT NULL,
+   PRIMARY KEY (`organization_id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=137;
+drop table if exists `{{params.reporting_database}}`.`patent_contractawardnumber`;
+CREATE TABLE `{{params.reporting_database}}`.`patent_contractawardnumber` (
+   `patent_id` varchar(24) NOT NULL,
+   `contract_award_number` varchar(255) Null,
+   PRIMARY KEY (`patent_id`,`contract_award_number`),
+   CONSTRAINT `patent_contractawardnumber_ibfk_1` FOREIGN KEY (`patent_id`) REFERENCES `{{params.reporting_database}}`.`government_interest` (`patent_id`) ON DELETE CASCADE
+ ) ENGINE=InnoDB;
+drop table if exists `{{params.reporting_database}}`.`patent_govintorg`;
+#CREATE TABLE IF NOT EXISTS `{{params.reporting_database}}`.`patent_govintorg` (
+#   `patent_id` varchar(255) NOT NULL,
+#   `organization_id` int(11) NOT NULL,
+#   PRIMARY KEY (`patent_id`,`organization_id`),
+#   KEY `organization_id` (`organization_id`),
+#   CONSTRAINT `patent_govintorg_ibfk_1` FOREIGN KEY (`patent_id`) REFERENCES `{{params.reporting_database}}`.`government_interest` (`patent_id`) ON DELETE CASCADE,
+#   CONSTRAINT `patent_govintorg_ibfk_2` FOREIGN KEY (`organization_id`) REFERENCES `{{params.reporting_database}}`.`government_organization` (`organization_id`) ON DELETE CASCADE
+# ) ENGINE=InnoDB;
+ 
+ CREATE TABLE IF NOT EXISTS `{{params.reporting_database}}`.`patent_govintorg` (
+   `patent_id` varchar(255) NOT NULL,
+   `organization_id` int(11) NOT NULL,
+   PRIMARY KEY (`patent_id`,`organization_id`),
+   KEY `organization_id` (`organization_id`),
+   CONSTRAINT `patent_govintorg_ibfk_2` FOREIGN KEY (`organization_id`) REFERENCES `{{params.reporting_database}}`.`government_organization` (`organization_id`) ON DELETE CASCADE
+ ) ENGINE=InnoDB;
+ 
+ 
+ 
+
+INSERT INTO `{{params.reporting_database}}`.`government_interest` SELECT * FROM `{{params.raw_database}}`.`government_interest`;
+INSERT INTO `{{params.reporting_database}}`.`government_organization` SELECT * FROM `{{params.raw_database}}`.`government_organization`;
+INSERT INTO `{{params.reporting_database}}`.`patent_contractawardnumber` SELECT * FROM `{{params.raw_database}}`.`patent_contractawardnumber`;
+drop table if exists `{{params.raw_database}}`.`temp_patent_govintorg_backup`;
+alter table `{{params.raw_database}}`.`patent_govintorg` rename `{{params.raw_database}}`.`temp_patent_govintorg_backup`;
+
+INSERT INTO `{{params.reporting_database}}`.`patent_govintorg` SELECT * FROM `{{params.raw_database}}`.`patent_govintorg`;
+
+ALTER TABLE `{{params.reporting_database}}`.`government_organization` ADD INDEX `ix_government_organization_name`(`name`);
+ALTER TABLE `{{params.reporting_database}}`.`government_organization` ADD INDEX `ix_government_organization_level_one`(`level_one`);
+ALTER TABLE `{{params.reporting_database}}`.`government_organization` ADD INDEX `ix_government_organization_level_two`(`level_two`);
+ALTER TABLE `{{params.reporting_database}}`.`government_organization` ADD INDEX `ix_government_organization_level_three`(`level_three`);
+
+# END Government interest tables
