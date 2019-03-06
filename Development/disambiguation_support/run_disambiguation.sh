9d4a20ec7b6cc7192445be33e07a95172743acd5
sarahkelley <skelley@air.org>
2019-03-06 16:37:59 +0000

    Reorganizing disambiguation driver

--- Development/disambiguation_support/run_disambiguation.sh
+++ Development/disambiguation_support/run_disambiguation.sh
@@ -3,4 +3,4 @@
 KEYFILE="$1"
 
 #run disambiguation
-ssh -i "$KEYFILE" centos@ec2-52-21-62-204.compute-1.amazonaws.com "cd /data/inventor-disambiguation-internal; bash run_inventor.sh > disambig.out"
+ssh -i "$KEYFILE" disambiguser@ec2-52-21-62-204.compute-1.amazonaws.com "cd /data/inventor-disambiguation-internal; bash run_all.sh > disambig.out"
