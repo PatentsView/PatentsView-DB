8a99ab291a26f93af4f11f6f9ade35e8d5ca2343
sarahkelley <skelley@air.org>
2019-03-25 22:43:35 +0000

    Fixed slack messaging

--- Scripts/Website_Database_Generator/database/validate_query.py
+++ Scripts/Website_Database_Generator/database/validate_query.py
@@ -4,11 +4,14 @@ from . import db_con
 from Development.helpers import database_helpers
 from Development.helpers import general_helpers
 import pprint
+import configparser
+import os
 
 
-def validate_and_execute(filename, **context):
+
+def validate_and_execute(filename, slack_client, slack_channel, **context):
     general_helpers.send_slack_notification(
-        "Executing Query File: `" + filename + "`", "info")
+        "Executing Query File: `" + filename + "`", slack_client, slack_channel, section="Reporting DB Update", level="info")
     sql_content = context['templates_dict']['source_sql']
     sql_statements = sqlparse.split(sql_content)
     for sql_statement in sql_statements:
@@ -30,7 +33,7 @@ def validate_and_execute(filename, **context):
             if parsed_statement.get_type().lower() == 'insert':
                 if not database_helpers.check_query_plan(db_con, single_line_query):
                     message = "Query execution plan involves full table scan: ```" + single_line_query + "```"
-                    general_helpers.send_slack_notification(message, "warning")
+                    general_helpers.send_slack_notification(message, slack_client, slack_channel, section="Reporting DB Update", level="warning")
                     # raise Exception(message)
                 table_finder_1 = re.compile("`PatentsView_[0-9]{8}[^`]*`.`[^`]+`")
                 tables_1 = table_finder_1.findall(single_line_query)
@@ -42,7 +45,7 @@ def validate_and_execute(filename, **context):
                     collation_check_parameters += [x.replace("`", "") for x in table.split(".")]
                 if not database_helpers.check_encoding_and_collation(db_con, collation_check_parameters):
                     message = "Character set and/or collation mismatch between tables involved in query :```" + single_line_query + "```"
-                    general_helpers.send_slack_notification(message, "warning")
+                    general_helpers.send_slack_notification(message, slack_client, slack_channel, section="Reporting DB Update", level="warning")
                     raise Exception(message)
             # general_helpers.send_slack_notification(
             #     "Executing Query : ```" + single_line_query + "```", "info")
@@ -51,9 +54,9 @@ def validate_and_execute(filename, **context):
                     db_con.execute(single_line_query)
                 except Exception as e:
                     general_helpers.send_slack_notification(
-                        "Execution of Query failed: ```" + single_line_query + "```", "error")
+                        "Execution of Query failed: ```" + single_line_query + "```", slack_client, slack_channel, section="Reporting DB Update", level="error")
                     raise e
         print(single_line_query)
-        # db_con.execute(single_line_query)
+        db_con.execute(single_line_query)
     general_helpers.send_slack_notification(
-        "Execution for Query File: `" + filename + "` is complete", "success")
+        "Execution for Query File: `" + filename + "` is complete", slack_client, slack_channel, section="Reporting DB Update", level="success")
