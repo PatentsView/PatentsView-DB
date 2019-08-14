6d703ffac4755d1714aac4e524bddfb34e6c7b47
Nisha <npatel@.air.org>
2019-08-14 11:07:58 -0400

    added flushed print statements, reworked code to chunk write_inventor(), update_raw(), and upload_raw() functions

--- Development/post_process_disambiguation/post_process_inventor.py
+++ Development/post_process_disambiguation/post_process_inventor.py
@@ -35,34 +35,84 @@ def make_lookup(disambiguated_folder):
 
 
 def write_inventor(inventors_to_write, disambiguated_folder):
+
     inventor_list = []
-    print("processing start")
+
     for inv_id, inventor in inventors_to_write.items():
         inventor_list.append([inv_id] + inventor)
+
     inventor_data = pd.DataFrame(inventor_list)
     inventor_data.columns = ['id', 'name_first', 'name_last']
-    start = time.time()
-    inventor_data.to_sql(con=db_con, name='inventor', if_exists='append', index=False, method="multi")
-    print("TO SQL: Time")
-    end = time.time()
-    print(end - start)
+
+    # steps: write to output, clear df, read in chunks
     inventor_data.to_csv("{}/inventor.csv".format(disambiguated_folder), index=False)
+    del inventor_data
+
+    print("processed inventor list, now chunk inserting", flush=True)
+
+    inv_chunk_size = 300000
+    data_chunk = pd.read_csv("{}/inventor.csv".format(disambiguated_folder), encoding='utf-8', chunksize=inv_chunk_size)
+
+    start = time.time()
+    batch_counter = 0
+    for chunk in tqdm.tqdm(data_chunk, desc='inventor table - batch inserts: '+ str(batch_counter)):
+        chunk.to_sql(con=db_con, name='inventor', if_exists='append', index=False, method="multi")
+        batch_counter += 1
+
+    print("TO SQL: Time", flush=True)
+    end = time.time()
+    print(end - start, flush=True)
 
 
 def update_raw(db_con, disambiguated_folder, lookup):
-    raw_inventor = db_con.execute("select * from rawinventor")
-    print('got raw inventor')
+
+    print("opening rawinventor updated for writing...", flush=True)
+
     output = csv.writer(open(disambiguated_folder + "/rawinventor_updated.csv", 'w'), delimiter='\t')
     output.writerow(
         ['uuid', 'patent_id', 'inventor_id', 'rawlocation_id', 'name_first', 'name_last', 'sequence', 'rule_47'])
-    for row in raw_inventor:
-        inventor_id = lookup[row[0]]
-        output.writerow(
-            [row['uuid'], row['patent_id'], inventor_id, row['rawlocation_id'], row['name_first'], row['name_last'],
-             row['sequence'], row['rule_47']])
+
+    limit = 300000
+    offset = 0
+    batch_counter = 0
+
+    print("now batch processing", flush=True)
+
+    while True:
+        batch_counter += 1
+        print('Next iteration...', flush=True)
+
+        # order by with primary key column - this has no nulls
+        raw_inv_chunk = db_con.execute("select * from rawinventor order by uuid limit {0} offset {1}".format(limit, offset))
+
+        counter = 0
+        for row in tqdm.tqdm(raw_inv_chunk, total=limit,
+                             desc="rawinventor - batch:" + str(batch_counter)):
+
+            inventor_id = lookup[row[0]]
+            output.writerow(
+                [row['uuid'], row['patent_id'], inventor_id, row['rawlocation_id'], row['name_first'], row['name_last'],
+                 row['sequence'], row['rule_47']])
+
+            # keep going because we have chunks to process
+            counter += 1
+
+        # means we have no more batches to process
+        if counter == 0:
+            break
+
+        offset = offset + limit
+
+
+    print("done updating rawinventor...", flush=True)
+
+
 
 ## Keeping this method separate to avoid memory issues
 def upload_raw(db_con,disambiguated_folder, db) :
+
+    print("now creating index syntax", flush=True)
+
     db_con.execute('create table rawinventor_inprogress like rawinventor')
     add_indexes_fetcher=db_con.execute("SELECT CONCAT('ALTER TABLE `',TABLE_NAME,'` ','ADD ', IF(NON_UNIQUE = 1, CASE UPPER(INDEX_TYPE) WHEN 'FULLTEXT' THEN 'FULLTEXT INDEX' WHEN 'SPATIAL' THEN 'SPATIAL INDEX' ELSE CONCAT('INDEX `', INDEX_NAME, '` USING ', INDEX_TYPE ) END, IF(UPPER(INDEX_NAME) = 'PRIMARY', CONCAT('PRIMARY KEY USING ', INDEX_TYPE ), CONCAT('UNIQUE INDEX `', INDEX_NAME, '` USING ', INDEX_TYPE ) ) ), '(', GROUP_CONCAT( DISTINCT CONCAT('`', COLUMN_NAME, '`') ORDER BY SEQ_IN_INDEX ASC SEPARATOR ', ' ), ');' ) AS 'Show_Add_Indexes' FROM information_schema.STATISTICS WHERE TABLE_SCHEMA = '"+db+"' AND TABLE_NAME='rawinventor_inprogress' and UPPER(INDEX_NAME) <> 'PRIMARY' GROUP BY TABLE_NAME, INDEX_NAME ORDER BY TABLE_NAME ASC, INDEX_NAME ASC; ")
     add_indexes=add_indexes_fetcher.fetchall()
@@ -72,18 +122,31 @@ def upload_raw(db_con,disambiguated_folder, db) :
 
     for drop_sql in drop_indexes:
         db_con.execute(drop_sql[0])
+
     start = time.time()
-    raw_inventor= pd.read_csv(disambiguated_folder + "/rawinventor_updated.csv", encoding = 'utf-8', delimiter = '\t', keep_default_na=False)
-    raw_inventor.sort_values(by="uuid", inplace=True)
-    raw_inventor.reset_index(inplace=True, drop=True)
-    n = 10000  # chunk row size
-    for i in tqdm.tqdm(range(0, raw_inventor.shape[0], n),  total=raw_inventor.shape[0]/n, desc="Raw Inventor Chunk"):
-        current_chunk = raw_inventor[i:i + n]
-        with db_con.begin() as conn:
-            current_chunk.to_sql(con=conn, name='rawinventor_inprogress', index=False, if_exists='append',
-                                 method="multi")  # append keeps the index
+
+    print("Now chunk processing", flush=True)
+
+    inv_chunk_size = 300000
+    data_chunk = pd.read_csv(disambiguated_folder + "/rawinventor_updated.csv", encoding='utf-8', delimiter='\t',
+                               keep_default_na=False, chunksize=inv_chunk_size)
+
+    batch_counter = 0
+    for chunk in tqdm.tqdm(data_chunk, desc='desc=rawinventor_inprogress - batch: ' + str(batch_counter)):
+
+        chunk.sort_values(by="uuid", inplace=True)
+        chunk.reset_index(inplace=True, drop=True)
+
+        chunk.to_sql(con=db_con, name='rawinventor_inprogress', if_exists='append', index=False, method="multi")
+        batch_counter += 1
+
+    print("TO SQL: Time", flush=True)
     end = time.time()
-    print("Load Time:" + str(round(end - start)))
+
+    print("Load Time: " + str(round(end - start)), flush=True)
+
+    print("now adding indexes", flush=True)
+
     for add_sql in add_indexes:
         print(add_sql[0])
         db_con.execute(add_sql[0])
@@ -102,30 +165,36 @@ if __name__ == '__main__':
                                            config['DATABASE']['PASSWORD'], config['DATABASE']['NEW_DB'])
     disambiguated_folder = "{}/disambig_output".format(config['FOLDERS']['WORKING_FOLDER'])
 
+    print('making lookup', flush=True)
     start = time.time()
     lookup, inventors = make_lookup(disambiguated_folder)
     end = time.time()
-    print('done lookup ')
-    print("Time Taken:")
-    print(end - start)
+    print('done lookup', flush=True)
+    print("Time Taken:", flush=True)
+    print(end - start, flush=True)
 
+    print('writing inventor', flush=True)
     start = time.time()
     write_inventor(inventors, disambiguated_folder)
     end = time.time()
-    print('written inventor')
-    print("Write Total Time")
-    print(end - start)
+    print('written inventor', flush=True)
+    print("Write Total Time", flush=True)
+    print(end - start, flush=True)
 
+
+    print("starting update raw", flush=True)
     start = time.time()
     update_raw(db_con, disambiguated_folder, lookup)
     end = time.time()
-    print('written rawinventor')
-    print("Write Total Time")
-    print(end - start)
+    print('finished update raw', flush=True)
+    print("Total Time", flush=True)
+    print(end - start, flush=True)
 
+
+    print("starting upload raw", flush=True)
     start = time.time()
     upload_raw(db_con, disambiguated_folder, config['DATABASE']['TEMP_UPLOAD_DB'])
     end = time.time()
-    print('written rawinventor')
-    print("Write Total Time")
-    print(end - start)
+    print('finished upload raw', flush=True)
+    print("Write Total Time", flush=True)
+    print(end - start, flush=True)
