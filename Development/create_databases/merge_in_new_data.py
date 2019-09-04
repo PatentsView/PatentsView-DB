f375e98e4d15bf7fae8c301c304d7ddadbbd3101
Nisha <npatel@.air.org>
2019-09-04 15:07:48 -0400

    added missing government_interest table to table list

--- Development/create_databases/merge_in_new_data.py
+++ Development/create_databases/merge_in_new_data.py
@@ -26,7 +26,7 @@ command = "select table_name from information_schema.tables where table_type = '
 tables_data = engine.execute(command)
 tables = [table['table_name'] for table in tables_data]
 tables = ['application', 'botanic', 'brf_sum_text', 'claim', 'detail_desc_length', 'draw_desc_text', 'figures', 'foreigncitation',
-          'foreign_priority', 'ipcr', 'mainclass', 'non_inventor_applicant' ,'otherreference', 'patent', 'rawassignee', 'rawexaminer',
+          'foreign_priority', 'government_interest', 'ipcr', 'mainclass', 'non_inventor_applicant' ,'otherreference', 'patent', 'rawassignee', 'rawexaminer',
           'pct_data','rawinventor', 'rawlawyer', 'rawlocation', 'rel_app_text', 'subclass', 'us_term_of_grant',
           'usapplicationcitation', 'uspatentcitation', 'usreldoc']
 engine.execute("SET FOREIGN_KEY_CHECKS=0;")
