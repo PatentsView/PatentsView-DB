cc8da1a4eab7acb99fe4d486bf8b0d2d22ba8e86
patenish <43384682+patenish@users.noreply.github.com>
2019-11-04 11:05:31 -0500

    fixed string format comparison of expected db end dates (#58)

--- Development/QA/02_date.py
+++ Development/QA/02_date.py
@@ -1,6 +1,7 @@
 #Date
 import pandas as pd
 import os
+import re
 import configparser
 import sys
 project_home = os.environ['PACKAGE_HOME']
@@ -22,17 +23,22 @@ latest_expected_date = config['DATES']['END_DATE']
 engine = general_helpers.connect_to_db(host, username, password, new_database)
 data = pd.read_csv("{}/01_latest_date_check.csv".format(previous_qa_loc))
 
-def check_latest_date(newdb, last_date):
+def check_latest_date(new_database, latest_expected_date):
 	date_dict = {}
 	var = engine.execute("select max(date) from {0}.patent;".format(new_database))
 	found_date = [row[0] for row in var][0]
-	if str(found_date) != str(latest_expected_date):
-		date_error =  "The latest date is {0}, and it should be {1}".format(str(found_date), str(latest_expected_date))
+
+	# substitute out - for comparing database timestamps
+	found_date = re.sub('-', '', str(found_date))
+
+	if found_date != latest_expected_date:
+		date_error =  "The latest date is {0}, and it should be {1}".format(found_date, latest_expected_date)
 		date_dict.update({new_database:date_error})
 	else:
 		date_match = "date matches"
 		date_dict.update({new_database:date_match})
 	return date_dict
+
 def date_check_to_file(previous_qa_loc, date_dict, new_qa_loc):
 	df = pd.read_csv('{}/01_latest_date_check.csv'.format(previous_qa_loc))
 	df_2 = pd.DataFrame.from_dict(date_dict, orient = 'index')
