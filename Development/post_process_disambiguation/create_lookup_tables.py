0d4dc764c0e102ecc85d0f5e9e17b38389857b88
DiPietroch <dipietro.christopher@gmail.com>
2020-02-04 15:03:42 -0500

    Changed SQL statements for assingee location and inventor location (#74)

--- Development/post_process_disambiguation/create_lookup_tables.py
+++ Development/post_process_disambiguation/create_lookup_tables.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 import pandas as pd
@@ -28,13 +27,13 @@ db_con.execute('insert ignore into patent_lawyer select patent_id, lawyer_id fro
 print('patent_lawyer')
 db_con.execute("create table temp_assignee_loc as select assignee_id, rawlocation_id, location_id_transformed as location_id from rawassignee r left join rawlocation l on r.rawlocation_id = l.id;")
 print('made locaiton_assignee_temp')
-db_con.execute("insert ignore into location_assignee (select max(location_id) as location_id, a.assignee_id from (select assignee_id, max(location_id_count) as max_count from (select assignee_id, location_id, count(location_id) as location_id_count from temp_assignee_loc group by assignee_id, location_id) as m group by assignee_id) as a left join (select assignee_id, location_id, count(location_id) as location_id_count from temp_assignee_loc group by assignee_id, location_id) as b on a.assignee_id =b.assignee_id and a.max_count=b.location_id_count group by a.assignee_id);")
+db_con.execute("insert ignore into location_assignee SELECT distinct rl.location_id, ri.assignee_id from rawassignee ri left join rawlocation rl on rl.id = ri.rawlocation_id where ri.assignee_id is not NULL and rl.location_id is not NULL;")
 print('location_assignee')
 #I don't understand why location assignee and location inventor have different types of location id
 db_con.execute("create table temp_inventor_loc as select inventor_id, location_id from rawinventor r left join rawlocation l on r.rawlocation_id = l.id;")
 print('done inventor temp')
 
-db_con.execute("insert ignore into location_inventor (select max(location_id) as location_id, a.inventor_id from (select inventor_id, max(location_id_count) as max_count from (select inventor_id, location_id, count(location_id) as location_id_count from temp_inventor_loc where location_id !='' and location_id is not null group by inventor_id, location_id) as m group by inventor_id) as a left join (select inventor_id, location_id, count(location_id) as location_id_count from temp_inventor_loc where location_id !='' and location_id is not null group by inventor_id, location_id) as b on a.inventor_id =b.inventor_id and a.max_count=b.location_id_count group by a.inventor_id);")
+db_con.execute("insert ignore into location_inventor SELECT rl.location_id, ri.inventor_id from rawinventor ri left join rawlocation rl on rl.id = ri.rawlocation_id where ri.inventor_id is not NULL and rl.location_id is not NULL;")
 print('location_inventor')
 
 
