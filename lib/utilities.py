f58a2dc92097a2f244aa30a6c694bdc8a975ebd3
Sarvothaman Madhavan <smadhavan@air.org>
2020-04-02 21:44:20 -0400

    Fix weekday count

--- lib/utilities.py
+++ lib/utilities.py
@@ -4,7 +4,7 @@ import calendar
 
 def weekday_count(start_date, end_date):
     week = {}
-    for i in range((end_date - start_date).days):
-        day = calendar.day_name[(start_date + datetime.timedelta(days=i + 1)).weekday()]
+    for i in range((end_date - start_date).days + 1):
+        day = calendar.day_name[(start_date + datetime.timedelta(days=i)).weekday()]
         week[day] = week[day] + 1 if day in week else 1
     return week
