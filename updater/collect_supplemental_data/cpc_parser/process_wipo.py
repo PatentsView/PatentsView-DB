cb1663c6958e2a761b73295f8d73554ef9a6d6db
Sarvothaman Madhavan <smadhavan@air.org>
2020-04-24 20:04:28 -0400

    Fixed and optimized wipo process

--- updater/collect_supplemental_data/cpc_parser/process_wipo.py
+++ updater/collect_supplemental_data/cpc_parser/process_wipo.py
@@ -1,11 +1,45 @@
+import os
 import time
 import pandas as pd
 from sqlalchemy import create_engine
+from memory_profiler import profile
 
-from lib.configuration import get_connection_string
+from lib.configuration import get_connection_string, get_config
 
 
-def extract_wipo_data(cpc_chunk, cpc_ipc_concordance, ipc_tech_map):
+def get_ipc_tech_code_field_map(ipc_tech_file):
+    # Read in the CSV file
+    ipc_technology_data = pd.read_csv(ipc_tech_file)
+    # Remove trailing % (likely wildcard format)
+    cleaned_ipc_tech_data = ipc_technology_data.assign(
+        IPC_Code_stripped=ipc_technology_data.IPC_code.str.replace("%", ""))
+    # Select and Rename columns
+    ipc_tech_map_frame = cleaned_ipc_tech_data[[
+        "IPC_Code_stripped", "Field_number"
+    ]].rename({"IPC_Code_stripped": "IPC_Code"}, axis=1)
+    # Convert to lookup dict
+    #     ipc_code_to_field_map = ipc_tech_map_frame.set_index(
+    #         'IPC_Code').to_dict()["Field_number"]
+    return ipc_tech_map_frame
+
+
+def get_ipc_cpc_ipc_concordance_map(concordance_file):
+    cpc_ipc_concordance_data = pd.read_csv(concordance_file,
+                                           header=None,
+                                           sep="\t")
+    cpc_ipc_concordance_data.columns = [
+        'cpc_code', 'unknown_column_1', 'ipc_code', 'unknown_column_2',
+        'unknown_column_3'
+    ]
+    cpc_ipc_known_data = cpc_ipc_concordance_data.drop(
+        ['unknown_column_1', 'unknown_column_2', 'unknown_column_3'], axis=1)
+    #     cpc_ipc_concordance_map = cpc_ipc_known_data.set_index(
+    #         'cpc_code').to_dict()["ipc_code"]
+    return cpc_ipc_known_data
+
+
+@profile()
+def extract_wipo_data(cpc_chunk, cpc_ipc_concordance, ipc_tech_map, config):
     # Obtain IPC Concordance for each patent based on cpc subgrou ID
     cpc_current_with_concordance = cpc_chunk.merge(right=cpc_ipc_concordance,
                                                    how='left',
@@ -28,11 +62,11 @@ def extract_wipo_data(cpc_chunk, cpc_ipc_concordance, ipc_tech_map):
         how='left',
         left_on='section',
         right_on='IPC_Code').drop('IPC_Code',
-                                  axis=1).rename({"Field_number": "wipo"},
+                                  axis=1).rename({"Field_number": "field_id"},
                                                  axis=1)
     # For failed lookups use "group" field
     secondary_lookup = cpc_current_with_wito_merge_1[
-        cpc_current_with_wito_merge_1.wipo.isnull()].merge(
+        cpc_current_with_wito_merge_1.field_id.isnull()].merge(
         right=ipc_tech_map,
         how='inner',
         left_on='group',
@@ -40,24 +74,24 @@ def extract_wipo_data(cpc_chunk, cpc_ipc_concordance, ipc_tech_map):
     # Merge the secondary lookup with main dataset (Merge by index)
     wipo_data_with_merge = cpc_current_with_wito_merge_1.join(
         secondary_lookup[["Field_number"]])
-    wipo_data_with_merge.wipo.fillna(wipo_data_with_merge.Field_number,
-                                     inplace=True)
+    wipo_data_with_merge.field_id.fillna(wipo_data_with_merge.Field_number,
+                                         inplace=True)
     # Clean UP
-    wipo_data = wipo_data_with_merge.dropna(subset=['wipo'], axis=0).drop(
+    wipo_data = wipo_data_with_merge.dropna(subset=['field_id'], axis=0).drop(
         [
             "subgroup_id", "category", "section_id", "subsection_id",
             "group_id", "uuid", "ipc_code", "section", "group", "Field_number"
         ],
         axis=1)
     # Counter for Each Field ID for each patent
-    wipo_count = wipo_data.groupby(["patent_number",
-                                    "wipo"]).size().to_frame('wipo_count')
+    wipo_count = wipo_data.groupby(["patent_id",
+                                    "field_id"]).size().to_frame('wipo_count')
     # Retain Top 3 most frequent Wipo field IDs
     wipo_filtered_data = wipo_count.sort_values("wipo_count").groupby(
-        "patent_number").head(3).reset_index()
+        "patent_id").head(3).reset_index()
     # Assign Sequence
-    wipo_filtered_data_sequenced = wipo_filtered_data.assign(
-        sequence=wipo_filtered_data.groupby(['patent_number']).cumcount())
+    wipo_filtered_data_sequenced = wipo_filtered_data.drop(["wipo_count"], axis=1).assign(
+        sequence=wipo_filtered_data.groupby(['patent_id']).cumcount())
     return wipo_filtered_data_sequenced
 
 
@@ -69,9 +103,9 @@ def upload_wipo(db_con, wipo_chunk):
     print("Chunk Load Time:" + str(round(end - start)))
 
 
-def wipo_chunk_processor(cpc_chunk, config):
+def wipo_chunk_processor(cpc_current_data, ipc_tech_field_map, cpc_ipc_concordance_map, config):
     engine = create_engine(get_connection_string(config, "TEMP_UPLOAD_DB"))
-    wipo_data = extract_wipo_data(cpc_chunk, config)
+    wipo_data = extract_wipo_data(cpc_current_data, cpc_ipc_concordance_map, ipc_tech_field_map, config)
     upload_wipo(engine, wipo_data)
 
 
@@ -84,7 +118,20 @@ def consolidate_wipo(config):
 
 def process_and_upload_wipo(config):
     myengine = create_engine(get_connection_string(config, "NEW_DB"))
-    limit = 300000
+    wipo_output = '{}/{}'.format(config['FOLDERS']['WORKING_FOLDER'],
+                                 'wipo_output')
+    if not os.path.exists(wipo_output):
+        os.mkdir(wipo_output)
+    persistent_files = config['FOLDERS']['PERSISTENT_FILES']
+    ipc_tech_file = '{}/ipc_technology.csv'.format(persistent_files)
+    ipc_tech_field_map = get_ipc_tech_code_field_map(ipc_tech_file)
+
+    concordance_file = '{}/{}/{}'.format(config['FOLDERS']['WORKING_FOLDER'],
+                                         'cpc_input', 'ipc_concordance.txt')
+
+    cpc_ipc_concordance_map = get_ipc_cpc_ipc_concordance_map(concordance_file)
+
+    limit = 1000000
     offset = 0
     batch_counter = 0
     while True:
@@ -94,8 +141,12 @@ def process_and_upload_wipo(config):
                                                  limit, offset))
         if cpc_current_data.shape[0] < 1:
             break
-        wipo_chunk_processor(cpc_current_data, config)
+        wipo_chunk_processor(cpc_current_data, ipc_tech_field_map, cpc_ipc_concordance_map, config)
         offset = offset + limit
+        break
+    # consolidate_wipo(config)
 
-    consolidate_wipo(config)
-    
+
+if __name__ == '__main__':
+    config = get_config()
+    process_and_upload_wipo(config)
