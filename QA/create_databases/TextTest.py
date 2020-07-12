bae80b81a360e64e457112cb28e029a882ccb1a1
Sarvothaman Madhavan <smadhavan@air.org>
2020-07-12 13:38:57 -0400

    Updated Text Tests Target

--- QA/create_databases/TextTest.py
+++ QA/create_databases/TextTest.py
@@ -6,21 +6,38 @@ from QA.PatentDatabaseTester import PatentDatabaseTester
 class TextMergeTest(PatentDatabaseTester):
     def __init__(self, config):
         end_date = datetime.datetime.strptime(config['DATES']['END_DATE'], '%Y%m%d')
+        brf_key = "brf_sum_text_{year}".format(year=end_date.year)
+        clm_key = "claim_{year}".format(year=end_date.year)
+        ddr_key = "detail_desc_text_{year}".format(year=end_date.year)
+        ddt_key = "draw_desc_text_{year}".format(year=end_date.year)
+
         super().__init__(config, 'TEXT_DATABASE', datetime.date(year=1976, month=1, day=1), end_date)
         self.table_config = {
-            "brf_sum_text": {'fields': {"uuid": {"null_allowed": False, "data_type": "varchar", "category": False},
-                                        "patent_id": {"null_allowed": False, "data_type": "varchar", "category": False},
-                                        "text": {"null_allowed": False, "data_type": "mediumtext", "category": False},
-                                        "version_indicator": {"null_allowed": False,
-                                                              "data_type": "varchar", "category": False}}},
-            "claim": {'fields': {"uuid": {"null_allowed": False, "data_type": "varchar", "category": False},
+            brf_key: {'fields': {"uuid": {"null_allowed": False, "data_type": "varchar", "category": False},
+                                 "patent_id": {"null_allowed": False, "data_type": "varchar", "category": False},
+                                 "text": {"null_allowed": False, "data_type": "mediumtext", "category": False},
+                                 "version_indicator": {"null_allowed": False,
+                                                       "data_type": "varchar", "category": False}}},
+            clm_key: {'fields': {"uuid": {"null_allowed": False, "data_type": "varchar", "category": False},
                                  "exemplary": {"null_allowed": True, "data_type": "varchar", "category": False},
                                  "patent_id": {"null_allowed": False, "data_type": "varchar", "category": False},
                                  "version_indicator": {"null_allowed": False, "data_type": "varchar",
                                                        "category": False},
                                  "text": {"null_allowed": False, "data_type": "mediumtext", "category": False},
                                  "dependent": {"null_allowed": True, "data_type": "varchar", "category": False},
-                                 "sequence": {"null_allowed": True, "data_type": "int", "category": False}}}}
+                                 "sequence": {"null_allowed": True, "data_type": "int", "category": False}}},
+            ddr_key: {
+                'fields': {"id": {"null_allowed": False, "data_type": "varchar", "category": False},
+                           "patent_id": {"null_allowed": False, "data_type": "varchar", "category": False},
+                           "text": {"null_allowed": False, "data_type": "mediumtext", "category": False},
+                           "sequence": {"null_allowed": False, "data_type": "int", "category": False},
+                           "version_indicator": {"null_allowed": True, "data_type": "varchar", "category": True}}},
+            ddt_key: {'fields': {"id": {"null_allowed": False, "data_type": "varchar", "category": False},
+                                 "patent_id": {"null_allowed": False, "data_type": "varchar", "category": False},
+                                 "text": {"null_allowed": False, "data_type": "mediumtext", "category": False},
+                                 "length": {"null_allowed": False, "data_type": "int", "category": False},
+                                 "version_indicator": {"null_allowed": True, "data_type": "varchar",
+                                                       "category": True}}}}
 
         self.patent_db_prefix = "`{db}`".format(db=self.config['DATABASE']['NEW_DB'])
 
