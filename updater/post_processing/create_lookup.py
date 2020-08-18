a81aa57b9e288745580d6d618fc7f95b21663fa9
Sarvothaman Madhavan <smadhavan@air.org>
2020-08-18 18:17:23 -0400

    Updated lookup generation to allow for missing location field

--- updater/post_processing/create_lookup.py
+++ updater/post_processing/create_lookup.py
@@ -3,15 +3,22 @@ from sqlalchemy import create_engine
 from lib.configuration import get_config, get_connection_string
 
 
-def load_lookup_table(config, entity):
+def load_lookup_table(config, entity, include_location=True):
     patent_entity_table = "patent_{entity}".format(entity=entity)
     entity_field = "{entity}_id".format(entity=entity)
     rawtable = "raw{entity}".format(entity=entity)
+    field_list = ["patent_id", "et.{ef}", "et.sequence"]
+    join_query = ""
+    if include_location:
+        field_list.append("rl.location_id")
+        join_query = "left join rawlocation rl on rl.id = et.rawlocation_id"
+    field_select = ", ".join(field_list).format(ef=entity_field)
     delete_query = "DELETE FROM {pet}".format(pet=patent_entity_table)
-    insert_query = "INSERT INTO {pet} SELECT et.patent_id, et.{ef}, et.sequence, rl.location_id from {rt} et left " \
-                   "join rawlocation " \
-                   "rl on rl.id = et.rawlocation_id where {ef} is not null".format(
-            pet=patent_entity_table, ef=entity_field,
+    insert_query = "INSERT IGNORE INTO {pet} SELECT {fs} from {rt} et {jq} where {ef} is not null".format(
+            fs=field_select,
+            jq=join_query,
+            pet=patent_entity_table,
+            ef=entity_field,
             rt=rawtable)
     engine = create_engine(get_connection_string(config, "NEW_DB"))
     with engine.begin() as cursor:
@@ -24,7 +31,7 @@ def load_lookup_table(config, entity):
 def create_lookup_tables(config):
     load_lookup_table(config, "inventor")
     load_lookup_table(config, "assignee")
-    load_lookup_table(config, "lawyer")
+    load_lookup_table(config, "lawyer", include_location=False)
 
 
 if __name__ == '__main__':
