8368bb3044ce0e9a8c4b4f9483cecb6c7481fca1
Sarvothaman Madhavan <smadhavan@air.org>
2020-10-01 20:17:59 -0400

    Updated Column Name in Trigger Creation

--- resources/text_table_triggers.sql
+++ resources/text_table_triggers.sql
@@ -2,19 +2,19 @@ CREATE TRIGGER before_insert_brf
   BEFORE INSERT
   ON temp_brf_sum_text
   FOR EACH row
-  SET new.id = uuid();
+  SET new.uuid = uuid();
 CREATE TRIGGER before_insert_claim
   BEFORE INSERT
   ON temp_claim
   FOR EACH row
-  SET new.id = uuid();
+  SET new.uuid = uuid();
 CREATE TRIGGER before_insert_ddt
   BEFORE INSERT
   ON temp_detail_desc_text
   FOR EACH row
-  SET new.id = uuid();
+  SET new.uuid = uuid();
 CREATE TRIGGER before_insert_drawdt
   BEFORE INSERT
   ON temp_draw_desc_text
   FOR EACH row
-  SET new.id = uuid();
+  SET new.uuid = uuid();
