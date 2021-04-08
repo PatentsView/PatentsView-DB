a612102ea6f6920fb1a290d937119df6d357a1f3
Sarvothaman Madhavan <smadhavan@air.org>
2021-04-08 15:53:36 -0400

    Test fix for empty line

--- QA/collect_supplemental_data/WithdrawnTest.py
+++ QA/collect_supplemental_data/WithdrawnTest.py
@@ -31,7 +31,7 @@ class WithdrawnTest:
         withdrawn_file = '{}/withdrawn.txt'.format(withdrawn_folder)
         with open(withdrawn_file, 'r') as f:
             for line in f.readlines():
-                if len(line)>0:
+                if len(line.strip())>0:
                     file_withdrawn_patents.append(xml_helpers.process_patent_numbers(line.strip('\n')))
         withdrawn_patent_query = "SELECT id from patent where withdrawn=1"
         if not self.connection.open:
