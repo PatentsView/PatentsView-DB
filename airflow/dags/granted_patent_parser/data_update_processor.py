f1a2fc8dcd830df98b567e61f92549932d613d74
Sarvothaman Madhavan <smadhavan@air.org>
2021-05-20 22:41:05 -0400

    Updated schedule to run later in the day

--- airflow/dags/granted_patent_parser/data_update_processor.py
+++ airflow/dags/granted_patent_parser/data_update_processor.py
@@ -34,7 +34,7 @@ data_updater = DAG(
         default_args=default_args,
         description='Start update process after verifying data collection is complete',
         start_date=datetime(2021, 3, 1),
-        schedule_interval='0 0 10 * 3,5',
+        schedule_interval='0 0 18 * 2,4',
         template_searchpath=templates_searchpath,
         catchup=True
         )
