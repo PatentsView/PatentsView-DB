c8a7fb609ac99af99d306d42a8c49337f9c5ad24
Sarvothaman Madhavan <smadhavan@air.org>
2021-06-08 11:08:24 -0400

    Updated Assignee Post Processing to include both pgpubs and granted persistent tables

--- airflow/dags/granted_patent_parser/assignee_post_processor.py
+++ airflow/dags/granted_patent_parser/assignee_post_processor.py
@@ -44,29 +44,62 @@ post_process_assignee_operator = PythonOperator(task_id='post_process_assignee',
                                                 on_success_callback=airflow_task_success,
                                                 on_failure_callback=airflow_task_failure)
 
-update_persistent_long_assignee = PythonOperator(
+update_granted_persistent_long_assignee = PythonOperator(
         task_id='update_persistent_long_assignee',
         python_callable=update_long_entity,
         op_kwargs={
                 'entity': 'assignee',
+                'database_type':'granted_patent'
                 },
         dag=assignee_post_processor
         )
 
-create_persistent_wide_assignee = PythonOperator(
+create_granted_persistent_wide_assignee = PythonOperator(
         task_id='create_persistent_wide_assignee',
         python_callable=write_wide_table,
         op_kwargs={
-                'entity': 'assignee'
+                'entity': 'assignee',
+                'database_type':'granted_patent'
                 },
         dag=assignee_post_processor
         )
 
-prepare_persistent_wide_assignee = PythonOperator(
+prepare_granted_persistent_wide_assignee = PythonOperator(
         task_id='prepare_persistent_wide_assignee',
         python_callable=prepare_wide_table,
         op_kwargs={
-                'entity': 'assignee'
+                'entity': 'assignee',
+                'database_type':'granted_patent'
+                },
+        dag=assignee_post_processor
+        )
+
+update_pregrant_persistent_long_assignee = PythonOperator(
+        task_id='update_persistent_long_assignee',
+        python_callable=update_long_entity,
+        op_kwargs={
+                'entity': 'assignee',
+                'database_type':'pgpubs'
+                },
+        dag=assignee_post_processor
+        )
+
+create_pregrant_persistent_wide_assignee = PythonOperator(
+        task_id='create_persistent_wide_assignee',
+        python_callable=write_wide_table,
+        op_kwargs={
+                'entity': 'assignee',
+                'database_type':'pgpubs'
+                },
+        dag=assignee_post_processor
+        )
+
+prepare_pregrant_persistent_wide_assignee = PythonOperator(
+        task_id='prepare_persistent_wide_assignee',
+        python_callable=prepare_wide_table,
+        op_kwargs={
+                'entity': 'assignee',
+                'database_type':'pgpubs'
                 },
         dag=assignee_post_processor
         )
@@ -77,9 +110,18 @@ qc_post_process_assignee_operator = PythonOperator(task_id='qc_post_process_assi
                                                    on_success_callback=airflow_task_success,
                                                    on_failure_callback=airflow_task_failure)
 dependency_list = {}
-dependency_list['main_dependency'] = [post_process_assignee_operator, update_persistent_long_assignee,
-                                      prepare_persistent_wide_assignee,
-                                      create_persistent_wide_assignee, qc_post_process_assignee_operator]
+dependency_list['granted_persistent'] = [
+        post_process_assignee_operator,
+        prepare_granted_persistent_wide_assignee,
+        update_granted_persistent_long_assignee,
+        create_granted_persistent_wide_assignee,
+        qc_post_process_assignee_operator]
+dependency_list['pgpubs_persistent'] = [
+        post_process_assignee_operator,
+        prepare_pregrant_persistent_wide_assignee,
+        update_pregrant_persistent_long_assignee,
+        create_pregrant_persistent_wide_assignee,
+        qc_post_process_assignee_operator]
 
 for dependency_group, dependency_sequence in dependency_list.items():
     chain_operators(dependency_sequence)
