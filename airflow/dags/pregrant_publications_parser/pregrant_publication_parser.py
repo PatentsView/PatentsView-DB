7f24f0033454443bc66599ea16b364d9f9a1f945
Sarvo Madhavan <smadhavan@air.org>
2021-11-13 23:35:12 -0500

    Updated pregrant publication database for celery

--- airflow/dags/pregrant_publications_parser/pregrant_publication_parser.py
+++ airflow/dags/pregrant_publications_parser/pregrant_publication_parser.py
@@ -9,26 +9,26 @@ from updater.xml_to_sql.parser import begin_parsing
 from updater.xml_to_sql.post_processing import begin_post_processing
 
 default_args = {
-        'owner':            'cdipietro',
-        'depends_on_past':  False,
-        'email':            ['contact@patentsview.org'],
-        'email_on_failure': False,
-        'email_on_retry':   False,
-        'retries':          3,
-        'retry_delay':      timedelta(minutes=5),
-        'concurrency':      4,
-
-        }
+    'owner': 'cdipietro',
+    'depends_on_past': False,
+    'email': ['contact@patentsview.org'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'concurrency': 4,
+    'queue': 'data_collector'
+}
 
 app_xml_dag = DAG(
-        dag_id='pregrant_publication_updater',
-        default_args=default_args,
-        description='Download and process application patent data and corresponding classifications data',
-        start_date=datetime(2021, 1, 7),
-        schedule_interval=timedelta(weeks=1),
-        catchup=True
-        # schedule_interval=None
-        )
+    dag_id='pregrant_publication_updater',
+    default_args=default_args,
+    description='Download and process application patent data and corresponding classifications data',
+    start_date=datetime(2021, 7, 1),
+    schedule_interval=timedelta(weeks=1),
+    catchup=True
+    # schedule_interval=None
+)
 
 create_database_operator = PythonOperator(task_id='create_pgpubs_database',
                                           python_callable=create_database,
@@ -41,14 +41,16 @@ parse_xml_operator = PythonOperator(task_id='parse_pgpubs_xml',
                                     python_callable=begin_parsing,
                                     dag=app_xml_dag,
                                     on_success_callback=airflow_task_success,
-                                    on_failure_callback=airflow_task_failure
+                                    on_failure_callback=airflow_task_failure,
+                                    pool='database_write_iops_contenders'
                                     )
 
 post_processing_operator = PythonOperator(task_id='post_process',
                                           python_callable=begin_post_processing,
                                           dag=app_xml_dag,
                                           on_success_callback=airflow_task_success,
-                                          on_failure_callback=airflow_task_failure
+                                          on_failure_callback=airflow_task_failure,
+                                          pool='database_write_iops_contenders'
                                           )
 
 merge_database_operator = PythonOperator(task_id='merge_database',
@@ -56,7 +58,8 @@ merge_database_operator = PythonOperator(task_id='merge_database',
                                          provide_context=True,
                                          dag=app_xml_dag,
                                          on_success_callback=airflow_task_success,
-                                         on_failure_callback=airflow_task_failure
+                                         on_failure_callback=airflow_task_failure,
+                                         pool='database_write_iops_contenders'
                                          )
 
 parse_xml_operator.set_upstream(create_database_operator)
