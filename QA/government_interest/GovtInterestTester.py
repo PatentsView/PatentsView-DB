cfea4675eb45ea850f70f69cad0faa77e7e92e01
bethanne-card <bcard@air.org>
2021-12-16 12:56:09 -0700

    used database info from the config rather than hardcoding

--- QA/government_interest/GovtInterestTester.py
+++ QA/government_interest/GovtInterestTester.py
@@ -117,14 +117,14 @@ FROM   `government_interest` gi
          ON pca.patent_id = gi.patent_id
        LEFT JOIN `patent_govintorg` pg
          ON pg.patent_id = gi.patent_id
-       LEFT JOIN patent.government_organization go
+       LEFT JOIN {raw_db}.government_organization go
          ON go.`organization_id` = pg.organization_id
            inner join 
            		(select id 
            		from patent p
            		       Join `government_interest` gi on p.id=gi.patent_id
            			   LEFT JOIN `patent_govintorg` pg ON pg.patent_id = p.id
-           		       LEFT JOIN patent.`government_organization` go ON go.`organization_id` = pg.organization_id
+           		       LEFT JOIN {raw_db}.`government_organization` go ON go.`organization_id` = pg.organization_id
            		{where_clause}
            		order by rand() 
            		limit 5) as rand_5_samples ON p.id=rand_5_samples.id;       
@@ -133,7 +133,7 @@ FROM   `government_interest` gi
         for date_clause, where_combination_type in where_combinations:
 
             where_clause = "AND ".join([date_clause, organization_wheres[where_combination_type]])
-            sampler_query = sampler_template.format(where_clause=where_clause)
+            sampler_query = sampler_template.format(where_clause=where_clause, raw_db=self.config['PATENTSVIEW_DATABASES']['RAW_DB'])
             with self.connection.cursor() as gov_int_cursor:
                 gov_int_cursor.execute(sampler_query)
                 for gov_int_row in gov_int_cursor:
