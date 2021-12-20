2449938bb97f6064d212c73e6a55cc02b4653a35
Sarvo Madhavan <smadhavan@air.org>
2021-12-19 22:18:26 -0500

    Updated config name

--- updater/post_processing/post_process_assignee.py
+++ updater/post_processing/post_process_assignee.py
@@ -89,7 +89,7 @@ def generate_disambiguated_assignees(update_config, engine, limit, offset):
 def create_assignee(update_config):
     engine = create_engine(get_connection_string(update_config, "RAW_DB"))
     version_indicator = update_config['DATES']['END_DATE']
-    suffix = config['DATES']['END_DATE']
+    suffix = update_config['DATES']['END_DATE']
     target_table = "assignee_{suffix}".format(suffix=suffix)
     create_sql = """
     CREATE TABLE `{target_table}` (
