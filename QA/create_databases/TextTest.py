9f232b325ff63a68c74a8e3c87f9b1734e4b5bd3
bethanne-card <bcard@air.org>
2022-02-07 09:53:31 -0700

    Testing to see if TextTest code is up to date in the 1-25 Patents DAG run

--- QA/create_databases/TextTest.py
+++ QA/create_databases/TextTest.py
@@ -7,6 +7,7 @@ from lib import utilities
 class TextMergeTest(DatabaseTester):
     def __init__(self, config):
         end_date = datetime.datetime.strptime(config['DATES']['END_DATE'], '%Y%m%d')
+        print(end_date)
         brf_key = "brf_sum_text_{year}".format(year=end_date.year)
         clm_key = "claims_{year}".format(year=end_date.year)
         ddr_key = "draw_desc_text_{year}".format(year=end_date.year)
@@ -14,6 +15,7 @@ class TextMergeTest(DatabaseTester):
         super().__init__(config, config['PATENTSVIEW_DATABASES']["TEXT_DB"], datetime.date(year=1976, month=1, day=1), end_date)
         keep_tables = []
         for i in self.table_config.keys():
+            print(i)
             if i in [brf_key, clm_key, ddr_key, ddt_key]:
                 keep_tables.append(i)
         self.table_config = utilities.with_keys(self.table_config, keep_tables)
@@ -43,7 +45,7 @@ if __name__ == '__main__':
     # config = get_current_config('granted_patent', **{
     #     "execution_date": datetime.date(2021, 10, 5)
     # })
-    config = get_current_config('pgpubs', **{
+    config = get_current_config('granted_patent', **{
         "execution_date": datetime.date(2021, 12, 2)
     })
     print(config['PATENTSVIEW_DATABASES']["TEMP_UPLOAD_DB"])
