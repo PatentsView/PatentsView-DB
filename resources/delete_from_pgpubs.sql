2b0471e48ed5706eaae2c1310270c92068147de3
John Butler <jbutler@air.org>
2022-02-22 11:16:11 -0500

    updates delete_from_pgpubs.sql
    replaces old insert statements with delete statements

--- resources/delete_from_pgpubs.sql
+++ resources/delete_from_pgpubs.sql
@@ -1,213 +1,41 @@
-{% set source_database = params.database %}
-    {% set dbdate= execution_date+macros.timedelta(days=7) %}
-    {% set year = dbdate.strftime('%Y') %}
-    {% if params.add_suffix %}
-    {% set source_database = source_database  +  dbdate.strftime('%Y%m%d')|string  %}
-    {% endif %}
-INSERT INTO pregrant_publications.publication SELECT * FROM `{{source_database}}`.publication;
-
-INSERT INTO pregrant_publications.application SELECT * FROM `{{source_database}}`.application;
-
-INSERT INTO pgpubs_text.brf_sum_text_2022 SELECT * FROM `{{source_database}}`.brf_sum_text_2022;
-
-INSERT INTO pgpubs_text.brf_sum_text_2021 SELECT * FROM `{{source_database}}`.brf_sum_text_2021;
-
-INSERT INTO pgpubs_text.brf_sum_text_2020 SELECT * FROM `{{source_database}}`.brf_sum_text_2020;
-
-INSERT INTO pgpubs_text.brf_sum_text_2019 SELECT * FROM `{{source_database}}`.brf_sum_text_2019;
-
-INSERT INTO pgpubs_text.brf_sum_text_2018 SELECT * FROM `{{source_database}}`.brf_sum_text_2018;
-
-INSERT INTO pgpubs_text.brf_sum_text_2017 SELECT * FROM `{{source_database}}`.brf_sum_text_2017;
-
-INSERT INTO pgpubs_text.brf_sum_text_2016 SELECT * FROM `{{source_database}}`.brf_sum_text_2016;
-
-INSERT INTO pgpubs_text.brf_sum_text_2015 SELECT * FROM `{{source_database}}`.brf_sum_text_2015;
-
-INSERT INTO pgpubs_text.brf_sum_text_2014 SELECT * FROM `{{source_database}}`.brf_sum_text_2014;
-
-INSERT INTO pgpubs_text.brf_sum_text_2013 SELECT * FROM `{{source_database}}`.brf_sum_text_2013;
-
-INSERT INTO pgpubs_text.brf_sum_text_2012 SELECT * FROM `{{source_database}}`.brf_sum_text_2012;
-
-INSERT INTO pgpubs_text.brf_sum_text_2011 SELECT * FROM `{{source_database}}`.brf_sum_text_2011;
-
-INSERT INTO pgpubs_text.brf_sum_text_2010 SELECT * FROM `{{source_database}}`.brf_sum_text_2010;
-
-INSERT INTO pgpubs_text.brf_sum_text_2009 SELECT * FROM `{{source_database}}`.brf_sum_text_2009;
-
-INSERT INTO pgpubs_text.brf_sum_text_2008 SELECT * FROM `{{source_database}}`.brf_sum_text_2008;
-
-INSERT INTO pgpubs_text.brf_sum_text_2007 SELECT * FROM `{{source_database}}`.brf_sum_text_2007;
-
-INSERT INTO pgpubs_text.brf_sum_text_2006 SELECT * FROM `{{source_database}}`.brf_sum_text_2006;
-
-INSERT INTO pgpubs_text.brf_sum_text_2005 SELECT * FROM `{{source_database}}`.brf_sum_text_2005;
-
-INSERT INTO pgpubs_text.brf_sum_text_2004 SELECT * FROM `{{source_database}}`.brf_sum_text_2004;
-
-INSERT INTO pgpubs_text.brf_sum_text_2003 SELECT * FROM `{{source_database}}`.brf_sum_text_2003;
-
-INSERT INTO pgpubs_text.brf_sum_text_2002 SELECT * FROM `{{source_database}}`.brf_sum_text_2002;
-
-INSERT INTO pgpubs_text.brf_sum_text_2001 SELECT * FROM `{{source_database}}`.brf_sum_text_2001;
-
-INSERT INTO pgpubs_text.claim_2022 SELECT * FROM `{{source_database}}`.claim_2022;
-
-INSERT INTO pgpubs_text.claim_2021 (id, document_number, text, sequence, dependent, filename, version_indicator, num) SELECT id, document_number, text, sequence, dependent, filename, version_indicator, num FROM `{{source_database}}`.claim_2021;
-
-INSERT INTO pgpubs_text.claim_2020 SELECT * FROM `{{source_database}}`.claim_2020;
-
-INSERT INTO pgpubs_text.claim_2019 SELECT * FROM `{{source_database}}`.claim_2019;
-
-INSERT INTO pgpubs_text.claim_2018 SELECT * FROM `{{source_database}}`.claim_2018;
-
-INSERT INTO pgpubs_text.claim_2017 SELECT * FROM `{{source_database}}`.claim_2017;
-
-INSERT INTO pgpubs_text.claim_2016 SELECT * FROM `{{source_database}}`.claim_2016;
-
-INSERT INTO pgpubs_text.claim_2015 SELECT * FROM `{{source_database}}`.claim_2015;
-
-INSERT INTO pgpubs_text.claim_2014 SELECT * FROM `{{source_database}}`.claim_2014;
-
-INSERT INTO pgpubs_text.claim_2013 SELECT * FROM `{{source_database}}`.claim_2013;
-
-INSERT INTO pgpubs_text.claim_2012 SELECT * FROM `{{source_database}}`.claim_2012;
-
-INSERT INTO pgpubs_text.claim_2011 SELECT * FROM `{{source_database}}`.claim_2011;
-
-INSERT INTO pgpubs_text.claim_2010 SELECT * FROM `{{source_database}}`.claim_2010;
-
-INSERT INTO pgpubs_text.claim_2009 SELECT * FROM `{{source_database}}`.claim_2009;
-
-INSERT INTO pgpubs_text.claim_2008 SELECT * FROM `{{source_database}}`.claim_2008;
-
-INSERT INTO pgpubs_text.claim_2007 SELECT * FROM `{{source_database}}`.claim_2007;
-
-INSERT INTO pgpubs_text.claim_2006 SELECT * FROM `{{source_database}}`.claim_2006;
-
-INSERT INTO pgpubs_text.claim_2005 SELECT * FROM `{{source_database}}`.claim_2005;
-
-INSERT INTO pgpubs_text.claim_2004 SELECT * FROM `{{source_database}}`.claim_2004;
-
-INSERT INTO pgpubs_text.claim_2003 SELECT * FROM `{{source_database}}`.claim_2003;
-
-INSERT INTO pgpubs_text.claim_2002 SELECT * FROM `{{source_database}}`.claim_2002;
-
-INSERT INTO pgpubs_text.claim_2001 SELECT * FROM `{{source_database}}`.claim_2001;
-
-INSERT INTO pregrant_publications.cpc SELECT * FROM `{{source_database}}`.cpc;
-
-INSERT INTO pgpubs_text.detail_desc_text_2022 SELECT * FROM `{{source_database}}`.detail_desc_text_2022;
-
-INSERT INTO pgpubs_text.detail_desc_text_2021 SELECT * FROM `{{source_database}}`.detail_desc_text_2021;
-
-INSERT INTO pgpubs_text.detail_desc_text_2020 SELECT * FROM `{{source_database}}`.detail_desc_text_2020;
-
-INSERT INTO pgpubs_text.detail_desc_text_2019 SELECT * FROM `{{source_database}}`.detail_desc_text_2019;
-
-INSERT INTO pgpubs_text.detail_desc_text_2018 SELECT * FROM `{{source_database}}`.detail_desc_text_2018;
-
-INSERT INTO pgpubs_text.detail_desc_text_2017 SELECT * FROM `{{source_database}}`.detail_desc_text_2017;
-
-INSERT INTO pgpubs_text.detail_desc_text_2016 SELECT * FROM `{{source_database}}`.detail_desc_text_2016;
-
-INSERT INTO pgpubs_text.detail_desc_text_2015 SELECT * FROM `{{source_database}}`.detail_desc_text_2015;
-
-INSERT INTO pgpubs_text.detail_desc_text_2014 SELECT * FROM `{{source_database}}`.detail_desc_text_2014;
-
-INSERT INTO pgpubs_text.detail_desc_text_2013 SELECT * FROM `{{source_database}}`.detail_desc_text_2013;
-
-INSERT INTO pgpubs_text.detail_desc_text_2012 SELECT * FROM `{{source_database}}`.detail_desc_text_2012;
-
-INSERT INTO pgpubs_text.detail_desc_text_2011 SELECT * FROM `{{source_database}}`.detail_desc_text_2011;
-
-INSERT INTO pgpubs_text.detail_desc_text_2010 SELECT * FROM `{{source_database}}`.detail_desc_text_2010;
-
-INSERT INTO pgpubs_text.detail_desc_text_2009 SELECT * FROM `{{source_database}}`.detail_desc_text_2009;
-
-INSERT INTO pgpubs_text.detail_desc_text_2008 SELECT * FROM `{{source_database}}`.detail_desc_text_2008;
-
-INSERT INTO pgpubs_text.detail_desc_text_2007 SELECT * FROM `{{source_database}}`.detail_desc_text_2007;
-
-INSERT INTO pgpubs_text.detail_desc_text_2006 SELECT * FROM `{{source_database}}`.detail_desc_text_2006;
-
-INSERT INTO pgpubs_text.detail_desc_text_2005 SELECT * FROM `{{source_database}}`.detail_desc_text_2005;
-
-INSERT INTO pgpubs_text.detail_desc_text_2004 SELECT * FROM `{{source_database}}`.detail_desc_text_2004;
-
-INSERT INTO pgpubs_text.detail_desc_text_2003 SELECT * FROM `{{source_database}}`.detail_desc_text_2003;
-
-INSERT INTO pgpubs_text.detail_desc_text_2002 SELECT * FROM `{{source_database}}`.detail_desc_text_2002;
-
-INSERT INTO pgpubs_text.detail_desc_text_2001 SELECT * FROM `{{source_database}}`.detail_desc_text_2001;
-
-INSERT INTO pgpubs_text.draw_desc_text_2022 SELECT * FROM `{{source_database}}`.draw_desc_text_2022;
-
-INSERT INTO pgpubs_text.draw_desc_text_2021 SELECT * FROM `{{source_database}}`.draw_desc_text_2021;
-
-INSERT INTO pgpubs_text.draw_desc_text_2020 SELECT * FROM `{{source_database}}`.draw_desc_text_2020;
-
-INSERT INTO pgpubs_text.draw_desc_text_2019 SELECT * FROM `{{source_database}}`.draw_desc_text_2019;
-
-INSERT INTO pgpubs_text.draw_desc_text_2018 SELECT * FROM `{{source_database}}`.draw_desc_text_2018;
-
-INSERT INTO pgpubs_text.draw_desc_text_2017 SELECT * FROM `{{source_database}}`.draw_desc_text_2017;
-
-INSERT INTO pgpubs_text.draw_desc_text_2016 SELECT * FROM `{{source_database}}`.draw_desc_text_2016;
-
-INSERT INTO pgpubs_text.draw_desc_text_2015 SELECT * FROM `{{source_database}}`.draw_desc_text_2015;
-
-INSERT INTO pgpubs_text.draw_desc_text_2014 SELECT * FROM `{{source_database}}`.draw_desc_text_2014;
-
-INSERT INTO pgpubs_text.draw_desc_text_2013 SELECT * FROM `{{source_database}}`.draw_desc_text_2013;
-
-INSERT INTO pgpubs_text.draw_desc_text_2012 SELECT * FROM `{{source_database}}`.draw_desc_text_2012;
-
-INSERT INTO pgpubs_text.draw_desc_text_2011 SELECT * FROM `{{source_database}}`.draw_desc_text_2011;
-
-INSERT INTO pgpubs_text.draw_desc_text_2010 SELECT * FROM `{{source_database}}`.draw_desc_text_2010;
-
-INSERT INTO pgpubs_text.draw_desc_text_2009 SELECT * FROM `{{source_database}}`.draw_desc_text_2009;
-
-INSERT INTO pgpubs_text.draw_desc_text_2008 SELECT * FROM `{{source_database}}`.draw_desc_text_2008;
-
-INSERT INTO pgpubs_text.draw_desc_text_2007 SELECT * FROM `{{source_database}}`.draw_desc_text_2007;
-
-INSERT INTO pgpubs_text.draw_desc_text_2006 SELECT * FROM `{{source_database}}`.draw_desc_text_2006;
-
-INSERT INTO pgpubs_text.draw_desc_text_2005 SELECT * FROM `{{source_database}}`.draw_desc_text_2005;
-
-INSERT INTO pgpubs_text.draw_desc_text_2004 SELECT * FROM `{{source_database}}`.draw_desc_text_2004;
-
-INSERT INTO pgpubs_text.draw_desc_text_2003 SELECT * FROM `{{source_database}}`.draw_desc_text_2003;
-
-INSERT INTO pgpubs_text.draw_desc_text_2002 SELECT * FROM `{{source_database}}`.draw_desc_text_2002;
-
-INSERT INTO pgpubs_text.draw_desc_text_2001 SELECT * FROM `{{source_database}}`.draw_desc_text_2001;
-
-INSERT INTO pregrant_publications.foreign_priority SELECT * FROM `{{source_database}}`.foreign_priority;
-
-INSERT INTO pregrant_publications.further_cpc SELECT * FROM `{{source_database}}`.further_cpc;
-
-INSERT INTO pregrant_publications.ipcr SELECT * FROM `{{source_database}}`.ipcr;
-
-INSERT INTO pregrant_publications.main_cpc SELECT * FROM `{{source_database}}`.main_cpc;
-
-INSERT INTO pregrant_publications.pct_data SELECT * FROM `{{source_database}}`.pct_data;
-
-INSERT INTO pregrant_publications.rawassignee (id, document_number, `sequence`, name_first, name_last, `organization`, type, rawlocation_id, city, state, country, filename, version_indicator) SELECT id, document_number, sequence, name_first, name_last, organization, type, rawlocation_id, city, state, country, filename,version_indicator FROM `{{source_database}}`.rawassignee;
-
-INSERT INTO pregrant_publications.rawinventor (id, document_number, name_first, name_last, `sequence`, designation, deceased, rawlocation_id, city, state, country, filename,version_indicator) SELECT id, document_number, name_first, name_last, sequence, designation, deceased, rawlocation_id, city, state, country, filename,version_indicator FROM `{{source_database}}`.rawinventor;
-
-INSERT INTO pregrant_publications.rawlocation (id, city, state, country, latitude, longitude, filename, version_indicator) SELECT id, city, state, country, latitude, longitude, filename,version_indicator FROM `{{source_database}}`.rawlocation;
-
-INSERT INTO pregrant_publications.rawuspc SELECT * FROM `{{source_database}}`.rawuspc;
-
-INSERT INTO pregrant_publications.rel_app_text SELECT * FROM `{{source_database}}`.rel_app_text;
-
-INSERT INTO pregrant_publications.us_parties (id, document_number, name_first, name_last, organization, type, designation, sequence, rawlocation_id, city, state, country, filename, version_indicator) SELECT id, document_number, name_first, name_last, organization, type, designation, sequence, rawlocation_id, city, state, country, filename, version_indicator FROM `{{source_database}}`.us_parties;
-
-INSERT INTO pregrant_publications.uspc SELECT * FROM `{{source_database}}`.uspc;
-
-INSERT INTO pregrant_publications.usreldoc SELECT * FROM `{{source_database}}`.usreldoc;
+# SET FOREIGN_KEY_CHECKS=0;
+
+delete from pregrant_publications.publication where version_indicator =@dbdate;
+delete from pregrant_publications.application where version_indicator =@dbdate;
+delete from pregrant_publications.cpc where version_indicator =@dbdate;
+delete from pregrant_publications.foreign_priority where version_indicator =@dbdate;
+delete from pregrant_publications.further_cpc where version_indicator =@dbdate;
+delete from pregrant_publications.ipcr where version_indicator =@dbdate;
+delete from pregrant_publications.main_cpc where version_indicator =@dbdate;
+delete from pregrant_publications.pct_data where version_indicator =@dbdate;
+delete from pregrant_publications.rawassignee where version_indicator =@dbdate;
+delete from pregrant_publications.rawinventor where version_indicator =@dbdate;
+delete from pregrant_publications.rawlocation where version_indicator =@dbdate;
+delete from pregrant_publications.rawuspc where version_indicator =@dbdate;
+delete from pregrant_publications.rel_app_text where version_indicator =@dbdate;
+delete from pregrant_publications.us_parties where version_indicator =@dbdate;
+delete from pregrant_publications.uspc where version_indicator =@dbdate;
+delete from pregrant_publications.usreldoc where version_indicator =@dbdate;
+delete from pgpubs_text.brf_sum_text_2021 where version_indicator =@dbdate;
+# delete from pgpubs_text.brf_sum_text_2022 where version_indicator =@dbdate;
+delete from pgpubs_text.claim_2021 where version_indicator =@dbdate;
+# delete from pgpubs_text.claim_2022 where version_indicator =@dbdate;
+# delete from pgpubs_text.detail_desc_text_2022 where version_indicator =@dbdate;
+delete from pgpubs_text.detail_desc_text_2021 where version_indicator =@dbdate;
+delete from pgpubs_text.draw_desc_text_2021 where version_indicator =@dbdate;
+# delete from pgpubs_text.draw_desc_text_2022 where version_indicator =@dbdate;
+
+delete from Patent_QA.DataMonitor_categorycount where update_version = @qavi and database_type = 'pgpubs';
+delete from Patent_QA.DataMonitor_count where update_version = @qavi and database_type = 'pgpubs';
+delete from Patent_QA.DataMonitor_floatingentitycount where update_version = @qavi and database_type = 'pgpubs';
+delete from Patent_QA.DataMonitor_govinterestsampler where update_version = @qavi and database_type = 'pgpubs';
+delete from Patent_QA.DataMonitor_locationcount where update_version = @qavi and database_type = 'pgpubs';
+delete from Patent_QA.DataMonitor_maxtextlength where update_version = @qavi and database_type = 'pgpubs';
+delete from Patent_QA.DataMonitor_nullcount where update_version = @qavi and database_type = 'pgpubs';
+delete from Patent_QA.DataMonitor_patentwithdrawncount where update_version = @qavi and database_type = 'pgpubs';
+delete from Patent_QA.DataMonitor_patentyearlycount where update_version = @qavi and database_type = 'pgpubs';
+delete from Patent_QA.DataMonitor_prefixedentitycount where update_version = @qavi and database_type = 'pgpubs';
+delete from Patent_QA.DataMonitor_topnentites where update_version = @qavi and database_type = 'pgpubs';
+
+
+# SET FOREIGN_KEY_CHECKS=1;
