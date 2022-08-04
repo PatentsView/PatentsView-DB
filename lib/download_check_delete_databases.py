e6a0b8e9bdb6c3cc6af082f3f384adb866623fd9
Beth Anne Card <bcard@air.org>
2022-08-04 17:39:07 -0600

    adding in defaults file for devuser

--- lib/download_check_delete_databases.py
+++ lib/download_check_delete_databases.py
@@ -106,11 +106,11 @@ def upload_tables_for_testing(config, db, table_list):
     elif db == 'pregrant_publications':
         output_path = "/PatentDataVolume/DatabaseBackups/PregrantPublications/pgpubs_db_tables"
     for table in table_list.split(","):
-        defaults_file = config['DATABASE_SETUP']['CONFIG_FILE']
+        # defaults_file = config['DATABASE_SETUP']['CONFIG_FILE']
         bash_command1 = f"gunzip -d {output_path}/{db}.{table}-schema.sql.gz"
         bash_command2 = f"gunzip -d {output_path}/{db}.{table}.sql.gz"
-        bash_command3 = f"mysql --defaults-file={defaults_file} -f {archive_db} < {output_path}/{db}.{table}-schema.sql"
-        bash_command4 = f"mysql --defaults-file={defaults_file} -f {archive_db} < {output_path}/{db}.{table}.sql"
+        bash_command3 = f"mysql --defaults-file=resources/sql.conf -f {archive_db} < {output_path}/{db}.{table}-schema.sql"
+        bash_command4 = f"mysql --defaults-file=resources/sql.conf -f {archive_db} < {output_path}/{db}.{table}.sql"
         bash_command5 = f"gzip {output_path}/{db}.{table}.sql"
         bash_command6 = f"gzip {output_path}/{db}.{table}-schema.sql"
         for i in [bash_command1, bash_command2, bash_command3, bash_command4, bash_command5, bash_command6]:
