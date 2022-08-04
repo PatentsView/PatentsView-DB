32c85a7cddc339f9c2cf3883fedd5c5f0302321b
Beth Anne Card <bcard@air.org>
2022-08-04 14:55:15 -0600

    debugging table archival script

--- lib/download_check_delete_databases.py
+++ lib/download_check_delete_databases.py
@@ -47,6 +47,7 @@ def subprocess_cmd(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     proc_stdout = process.communicate()[0].strip()
     print(proc_stdout)
+    process.wait()
 
 
 def backup_db(config, db):
