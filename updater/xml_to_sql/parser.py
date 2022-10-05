4ca45f109bd1e08009986279adf34cb36f435ca0
Jack Butler <jbutler@air.org>
2022-10-05 17:09:31 -0400

    add debugging prints for gi cleaning in weekly parser

--- updater/xml_to_sql/parser.py
+++ updater/xml_to_sql/parser.py
@@ -329,9 +329,12 @@ def load_df_to_sql(dfs, xml_file_name, config, log_queue, foreign_key_config):
         cols.remove(foreign_key_config["field_name"])
         dfs[df] = dfs[df].dropna(subset=cols, how='all')
         if df == 'government_interest':
+            print(f'g_i shape before n/a drop: {dfs[df].shape}')
             narows = dfs[df]['gi_statement'].str.contains(pat='not applicable', case=False)
             dfs[df].drop(index=narows, inplace=True)
+            print(f'g_i shape after n/a drop: {dfs[df].shape}')
             dfs[df]['gi_statement'] = dfs[df]['gi_statement'].str.strip()
+            print(f'g_i shape after strip: {dfs[df].shape}')
         dfs[df]['version_indicator'] = config['DATES']['END_DATE']
         try:
             dfs[df].to_sql(df, con=engine, if_exists='append', index=False)
