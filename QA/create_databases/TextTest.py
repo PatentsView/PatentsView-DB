122fb9fcedd1f595faa6232135bf207bf5eec812
Jack Butler <jbutler@air.org>
2022-10-07 19:19:28 -0400

    update pgpubs text QC class and corresponding config json

--- QA/create_databases/TextTest.py
+++ QA/create_databases/TextTest.py
@@ -30,10 +30,7 @@ class TextMergeTest(DatabaseTester):
         print(end_date)
         super().__init__(config, config['PATENTSVIEW_DATABASES']["TEXT_DB"], datetime.date(year=1976, month=1, day=1), end_date)
         brf_key = "brf_sum_text_{year}".format(year=end_date.year)
-        if config['PATENTSVIEW_DATABASES']["TEXT_DB"] == 'patent_text':
-            clm_key = "claims_{year}".format(year=end_date.year)
-        else:
-            clm_key = "claim_{year}".format(year=end_date.year)
+        clm_key = "claims_{year}".format(year=end_date.year)
         ddr_key = "draw_desc_text_{year}".format(year=end_date.year)
         ddt_key = "detail_desc_text_{year}".format(year=end_date.year)
         keep_tables = []
