09b8a10811d5d8ca9dc21fe2840b066ca23d9413
Beth Anne Card <bcard@air.org>
2023-01-21 10:32:19 -0700

    fixing version_indicator

--- reporting_database_generator/02_Patent.sql
+++ reporting_database_generator/02_Patent.sql
@@ -49,7 +49,7 @@ from
   left outer join `{{params.reporting_database}}`.`temp_id_mapping_location` tl on l.`id` =  tl.`old_location_id`
 where
   (ta.`new_assignee_id` is not null or
-  tl.`new_location_id` is not null) and  p.version_indicator<={{params.version_indicator}};
+  tl.`new_location_id` is not null) and  p.version_indicator<='{{params.version_indicator}}';
 
 
 drop table if exists `{{params.reporting_database}}`.`temp_patent_firstnamed_inventor`;
@@ -96,7 +96,7 @@ from
   left outer join `{{params.reporting_database}}`.`temp_id_mapping_location` tli on tli.`old_location_id` =  l.`id`
 where
   (ti.`new_inventor_id` is not null or
-  tli.`new_location_id` is not null)and  p.version_indicator<={{params.version_indicator}};
+  tli.`new_location_id` is not null)and  p.version_indicator<='{{params.version_indicator}}';
 
 
 drop table if exists `{{params.reporting_database}}`.`temp_num_foreign_documents_cited`;
@@ -114,7 +114,7 @@ insert into `{{params.reporting_database}}`.`temp_num_foreign_documents_cited`
 select
   `patent_id`, count(*)
 from
-  `{{params.raw_database}}`.`foreigncitation`  where version_indicator<={{params.version_indicator}}
+  `{{params.raw_database}}`.`foreigncitation`  where version_indicator<='{{params.version_indicator}}'
 group by
   `patent_id`;
 
@@ -134,7 +134,7 @@ insert into `{{params.reporting_database}}`.`temp_num_us_applications_cited`
 select
   `patent_id`, count(*)
 from
-  `{{params.raw_database}}`.`usapplicationcitation`  where version_indicator<={{params.version_indicator}}
+  `{{params.raw_database}}`.`usapplicationcitation`  where version_indicator<='{{params.version_indicator}}'
 group by
   `patent_id`;
 
@@ -155,7 +155,7 @@ insert into `{{params.reporting_database}}`.`temp_num_us_patents_cited`
 select
   `patent_id`, count(*)
 from
-  `{{params.raw_database}}`.`uspatentcitation`  where version_indicator<={{params.version_indicator}}
+  `{{params.raw_database}}`.`uspatentcitation`  where version_indicator<='{{params.version_indicator}}'
 group by
   `patent_id`;
 
@@ -177,7 +177,7 @@ select
 from
   `{{params.raw_database}}`.`uspatentcitation`
 where
-  `citation_id` is not null and `citation_id` != ''  and version_indicator<={{params.version_indicator}}
+  `citation_id` is not null and `citation_id` != ''  and version_indicator<='{{params.version_indicator}}'
 group by
   `citation_id`;
 
@@ -214,7 +214,7 @@ from
   left outer join `{{params.reporting_database}}`.`temp_num_foreign_documents_cited` t1 on t1.`patent_id` = p.`id`
   left outer join `{{params.reporting_database}}`.`temp_num_us_applications_cited` t2 on t2.`patent_id` = p.`id`
   left outer join `{{params.reporting_database}}`.`temp_num_us_patents_cited` t3 on t3.`patent_id` = p.`id`
-  left outer join `{{params.reporting_database}}`.`temp_num_times_cited_by_us_patents` t4 on t4.`patent_id` = p.`id`  where version_indicator<={{params.version_indicator}};
+  left outer join `{{params.reporting_database}}`.`temp_num_times_cited_by_us_patents` t4 on t4.`patent_id` = p.`id`  where version_indicator<='{{params.version_indicator}}';
 
 
 drop table if exists `{{params.reporting_database}}`.`temp_patent_earliest_application_date`;
@@ -234,7 +234,7 @@ select
 from
   `{{params.raw_database}}`.`application` a
 where
-  a.`date` is not null and a.`date` > date('1899-12-31') and a.`date` < date_add(current_date, interval 10 year)  and version_indicator<={{params.version_indicator}}
+  a.`date` is not null and a.`date` > date('1899-12-31') and a.`date` < date_add(current_date, interval 10 year)  and version_indicator<='{{params.version_indicator}}'
 group by
   a.`patent_id`;
 
@@ -256,7 +256,7 @@ select
 from
   `{{params.raw_database}}`.`patent` p
 where
-  p.`date` is not null and p.`date` > date('1899-12-31') and p.`date` < date_add(current_date, interval 10 year) and version_indicator<={{params.version_indicator}};
+  p.`date` is not null and p.`date` > date('1899-12-31') and p.`date` < date_add(current_date, interval 10 year) and version_indicator<='{{params.version_indicator}}';
 
 
 drop table if exists `{{params.reporting_database}}`.`patent`;
@@ -358,7 +358,7 @@ from
   left outer join `{{params.reporting_database}}`.`temp_patent_earliest_application_date` tpead on tpead.`patent_id` = p.`id`
   left outer join `{{params.raw_database}}`.`us_term_of_grant` ustog on ustog.`patent_id`=p.`id`
   left outer join `{{params.raw_database}}`.`detail_desc_length` ddl on ddl.`patent_id` = p.`id`
- where  p.version_indicator<={{params.version_indicator}};
+ where  p.version_indicator<='{{params.version_indicator}}';
 
 # END patent 
 
