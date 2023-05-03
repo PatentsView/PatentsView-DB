f59b5f556c127b344a360fa263e474cf13442f26
Jack Butler <jbutler@air.org>
2023-05-03 12:11:35 -0400

    update DownloadTest to accommodate multiple xml files in a week

--- QA/xml_to_csv/DownloadTest.py
+++ QA/xml_to_csv/DownloadTest.py
@@ -1,3 +1,5 @@
+import re
+
 class DownloadTest:
     def __init__(self, update_config):
         output_folder = '{working_folder}/raw_data'.format(working_folder=update_config['FOLDERS'][
@@ -6,9 +8,12 @@ class DownloadTest:
         xml_glob = "{folder}/*.xml".format(folder=output_folder)
         self.xml_files_count = 0
         self.filenames = []
+        self.dates = set()
         for fname in glob.glob(xml_glob):
             self.filenames.append(fname)
             self.xml_files_count += 1
+            filedate = re.match(r".*([0-9]{6})(_r\d)?", fname).group(1)
+            self.dates.add(filedate)
 
     def runTest(self, config):
         self.test_all_tuesdays(config)
@@ -20,7 +25,7 @@ class DownloadTest:
         start_date = datetime.datetime.strptime(config['DATES']['START_DATE'], '%Y%m%d')
         end_date = datetime.datetime.strptime(config['DATES']['END_DATE'], '%Y%m%d')
         week_day_counts = weekday_count(start_date, end_date)
-        assert week_day_counts['Tuesday'] == self.get_file_count()
+        assert week_day_counts['Tuesday'] == self.get_date_count()
 
     def test_file_sizes(self):
         import os
@@ -29,6 +34,9 @@ class DownloadTest:
 
     def get_file_count(self):
         return self.xml_files_count
+    
+    def get_date_count(self):
+        return(len(self.dates))
 
     def get_file_size(self):
         import os
