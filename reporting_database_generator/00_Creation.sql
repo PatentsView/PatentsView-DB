d9f5846c460c3c2844d3099e82fa81f7136a388e
Beth Anne Card <bcard@air.org>
2023-09-16 06:42:35 -0600

    testing new date for reporting DB

--- reporting_database_generator/00_Creation.sql
+++ reporting_database_generator/00_Creation.sql
@@ -1,1 +1,1 @@
-create database `PatentsView_{{ dag_run.logical_date | ds_nodash }}` default character set=utf8mb4 default collate=utf8mb4_unicode_ci;
+create database `PatentsView_{{ dag_run.end_date | ds_nodash }}` default character set=utf8mb4 default collate=utf8mb4_unicode_ci;
