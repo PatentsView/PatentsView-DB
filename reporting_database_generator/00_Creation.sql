95d137de282b5308adac87160200081e4656cb19
Beth Anne Card <bcard@air.org>
2023-09-20 15:41:37 -0600

    test

--- reporting_database_generator/00_Creation.sql
+++ reporting_database_generator/00_Creation.sql
@@ -1,2 +1,2 @@
-{% set reporting_database = PatentsView_{{ macros.ds_format(macros.ds_add(dag_run.data_interval_end | ds, -1), "%Y-%m-%d", "%Y%m%d") }} %}
+{% set reporting_database = PatentsView_ + macros.ds_format(macros.ds_add(dag_run.data_interval_end | ds, -1), "%Y-%m-%d", "%Y%m%d") %}
 create database `{{reporting_db}}` default character set=utf8mb4 default collate=utf8mb4_unicode_ci;
