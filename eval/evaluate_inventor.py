685b65834872fb5070f442801262f5cf011d4ed6
Beth Anne Card <bcard@air.org>
2023-10-06 10:22:50 -0600

    saving initial inventor eval work

--- eval/evaluate_inventor.py
+++ eval/evaluate_inventor.py
@@ -7,8 +7,53 @@ from er_evaluation.estimators import pairwise_precision_design_estimate, pairwis
 from er_evaluation.summary import cluster_sizes
 from pv_evaluation.benchmark import load_binette_2022_inventors_benchmark
 
+def get_data_for_inventor_summary_stats(config):
+    engine = create_engine(get_connection_string(config, "RAW_DB"))
+    pquery = "select * from patentsview_export_granted.g_persistent_inventor"
+    print(pquery)
+    patent = pd.read_sql_query(sql=pquery, con=engine)
+    patent.to_csv("patent.csv")
+    iquery = "select patent_id, inventor_sequence, raw_inventor_name_first, raw_inventor_name_last from patentsview_export_granted.g_inventor_not_disambiguated "
+    print(iquery)
+    persistent_inventor = pd.read_sql_query(sql=pquery, con=engine)
+    inventor_not_disambiguated = pd.read_sql_query(sql=iquery, con=engine)
 
-def evaluate_inventor_clustering(config, **kwargs):
+    inventor_not_disambiguated["mention_id"] = "US" + inventor_not_disambiguated.patent_id.astype(str) + "-" + inventor_not_disambiguated.inventor_sequence.astype(str)
+    inventor_not_disambiguated["name"] = inventor_not_disambiguated.raw_inventor_name_first + " " + inventor_not_disambiguated.raw_inventor_name_last
+    inventor_not_disambiguated.set_index("mention_id", inplace=True)
+    names = inventor_not_disambiguated["name"]
+    return persistent_inventor, names
+
+def evaluation_inventor_summary_stats(config):
+    qa_data = {
+        "DataMonitor_inventor_summary_stats": []
+    }
+    end_date = config["DATES"]["END_DATE"]
+    persistent_inventor, names = get_data_for_inventor_summary_stats(config)
+
+    from pv_evaluation.benchmark import inventor_summary_trend_plot
+    fig = inventor_summary_trend_plot(persistent_inventor, names)
+    fig.update_layout(
+        width=800,
+        height=300,
+        title="Summary Statistics"
+    )
+    fig['layout'].update(margin=dict(l=20, r=20, b=20, t=60))
+    fig.write_image("summary_trend.pdf")
+    fig.show(renderer="svg")
+    breakpoint()
+
+    # qa_data['DataMonitor_inventor_summary_stats'].append(
+    #     {
+    #         'update_version': end_date,
+    #         'precision': precision,
+    #         "p_std": p_std,
+    #         'recall': recall,
+    #         'r_std': r_std
+    #     })
+    # qa_data.to_sql(name="DataMonitor_inventor_summary_stats", if_exists='append', con=engine, index=False)
+
+def get_data_for_inventor_precision_recall(config):
     engine = create_engine(get_connection_string(config, "RAW_DB"))
     pquery = "select patent_id, patent_date from patentsview_export_granted.g_patent"
     print(pquery)
@@ -17,17 +62,35 @@ def evaluate_inventor_clustering(config, **kwargs):
     rquery = "select patent_id, sequence, inventor_id from patent.rawinventor "
     print(rquery)
     rawinventor = pd.read_sql_query(sql=rquery, con=engine)
-    rawinventor.to_csv("rawinventor.csv")
     patent_date = pd.DatetimeIndex(patent.patent_date)
     patent["patent_date"] = patent_date.year.astype(int)
     joined = rawinventor.merge(patent, on="patent_id", how="left")
-    joined["mention_id"] = "US" + joined.patent_id + "-" + str(joined.sequence)
+    joined["mention_id"] = "US" + joined.patent_id.astype(str) + "-" + joined.sequence.astype(str)
     joined = joined.query('patent_date >= 1975 and patent_date <= 2022')
-    breakpoint()
     current_disambiguation = joined.set_index("mention_id")["inventor_id"]
+    return current_disambiguation
 
-    pairwise_precision_design_estimate(current_disambiguation, load_binette_2022_inventors_benchmark(),
-                                       weights=1 / cluster_sizes(load_binette_2022_inventors_benchmark()))
+
+def evaluate_inventor_precision_recall(config):
+    qa_data = {
+        "DataMonitor_inventor_precision_recall": []
+    }
+    end_date = config["DATES"]["END_DATE"]
+    current_disambiguation = get_data_for_inventor_precision_recall(config)
+    # PRECISION & RECALL
+    precision, p_std = pairwise_precision_design_estimate(current_disambiguation, load_binette_2022_inventors_benchmark(), weights=1 / cluster_sizes(load_binette_2022_inventors_benchmark()))
+    recall, r_std =pairwise_recall_design_estimate(current_disambiguation, load_binette_2022_inventors_benchmark(), weights=1 / cluster_sizes(load_binette_2022_inventors_benchmark()))
+    qa_data['DataMonitor_inventor_precision_recall'].append(
+        {
+            'update_version': end_date,
+            'precision': precision,
+            "p_std": p_std,
+            'recall': recall,
+            'r_std': r_std
+        })
+    qa_connection_string = get_connection_string(config, database='QA_DATABASE', connection='APP_DATABASE_SETUP')
+    qa_engine = create_engine(qa_connection_string)
+    pd.DataFrame(qa_data['DataMonitor_inventor_precision_recall']).to_sql(name="DataMonitor_inventor_precision_recall", if_exists='append', con=qa_engine, index=False)
 
 
 if __name__ == '__main__':
@@ -36,5 +99,6 @@ if __name__ == '__main__':
         "execution_date": d
     })
     # config = get_current_config('granted_patent', schedule='quarterly', **kwargs)
-    evaluate_inventor_clustering(config)
+    # evaluate_inventor_precision_recall(config)
+    evaluation_inventor_summary_stats(config)
 
