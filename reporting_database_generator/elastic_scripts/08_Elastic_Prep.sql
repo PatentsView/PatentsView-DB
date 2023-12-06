d2d8d41a08902999ff60c5a078f30ff31d2629cf
Sarvothaman Madhavan <smadhavan@air.org>
2023-12-06 17:35:48 -0500

    Revert "Add sql and update dags for publications endpoint (#168)"
    This reverts commit b2c11b4ef0c660934317d0087d93ba5aa7a216d7.

--- reporting_database_generator/elastic_scripts/08_Elastic_Prep.sql
+++ reporting_database_generator/elastic_scripts/08_Elastic_Prep.sql
@@ -1,5 +1,3 @@
-{% set elastic_patent_db = "elastic_production_patent_" + macros.ds_format(macros.ds_add(dag_run.data_interval_end | ds, -1), "%Y-%m-%d", "%Y%m%d") %}
-{% set elastic_pgpub_db = "elastic_production_pgpub_" + macros.ds_format(macros.ds_add(dag_run.data_interval_end | ds, -1), "%Y-%m-%d", "%Y%m%d") %}
-create database `{{elastic_patent_db}}` default character set=utf8mb4 default collate=utf8mb4_unicode_ci;
-create database `{{elastic_pgpub_db}}` default character set=utf8mb4 default collate=utf8mb4_unicode_ci;
+{% set elastic_db = "elastic_production_" + macros.ds_format(macros.ds_add(dag_run.data_interval_end | ds, -1), "%Y-%m-%d", "%Y%m%d") %}
+create database `{{elastic_db}}` default character set=utf8mb4 default collate=utf8mb4_unicode_ci;
 
