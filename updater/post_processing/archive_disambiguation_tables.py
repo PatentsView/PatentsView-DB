76539878c2344ae045fd89a10208c5749a50d5ab
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2024-01-31 15:30:16 -0500

    making it so only granted_patent removes disambiguated_inventor_ids_

--- updater/post_processing/archive_disambiguation_tables.py
+++ updater/post_processing/archive_disambiguation_tables.py
@@ -10,7 +10,7 @@ def archive_assignee_tables(**kwargs):
 def archive_inventor_tables(**kwargs):
     for db_type in ['granted_patent', "pgpubs"]:
         run_table_archive(db_type, "inventor_disambiguation_mapping_", **kwargs)
-        run_table_archive(db_type, "disambiguated_inventor_ids_", **kwargs)
+    run_table_archive('granted_patent', "disambiguated_inventor_ids_", **kwargs)
     run_table_archive("granted_patent", "inventor_", **kwargs)
 
 def archive_location_tables(**kwargs):
