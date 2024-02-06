fd5bae0126c29c4cc8021b075fa2c25c8ba896e5
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2024-02-06 11:51:49 -0500

    updating code for archival of inventor_assignee_disambiguation task

--- updater/post_processing/archive_disambiguation_tables.py
+++ updater/post_processing/archive_disambiguation_tables.py
@@ -4,7 +4,7 @@ from lib.download_check_delete_databases import run_table_archive
 def archive_assignee_tables(**kwargs):
     for db_type in ['granted_patent', "pgpubs"]:
         run_table_archive(db_type, "assignee_disambiguation_mapping_", **kwargs)
-    run_table_archive(db_type, "disambiguated_assignee_ids_", **kwargs)
+    run_table_archive('granted_patent', "disambiguated_assignee_ids_", **kwargs)
     run_table_archive("granted_patent", "assignee_", **kwargs)
 
 def archive_inventor_tables(**kwargs):
