9f4e8de92b643daf5ceda0a5dd5b5f502be73fcc
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2024-03-26 16:05:10 -0400

    updating code for new QC checks for elasticsearch databases

--- QA/post_processing/ElasticDBTester.py
+++ QA/post_processing/ElasticDBTester.py
@@ -9,18 +9,19 @@ logger = logging.getLogger(__name__)
 
 
 class ElasticDBTester(DatabaseTester):
-    def __init__(self, config):
+    def  __init__(self, config):
         end_date = datetime.datetime.strptime(config['DATES']['END_DATE'], '%Y%m%d')
-        super().__init__(config, config['PATENTSVIEW_DATABASES']["REPORTING_DATABASE"], datetime.date(year=1976, month=1, day=1),end_date)
+        super().__init__(config, config['PATENTSVIEW_DATABASES']["ELASTICSEARCH_DB"], datetime.date(year=1976, month=1, day=1),end_date)
 
 
-def run_elastic_db_qa(**kwargs):
-    config = get_current_config('granted_patent', schedule="quarterly", **kwargs)
+def run_elastic_db_qa(db_type = None, **kwargs):
+    config = get_current_config(type = db_type, schedule="quarterly", **kwargs)
     qc = ElasticDBTester(config)
     qc.runReportingTests()
 
 
 if __name__ == '__main__':
-    config = get_current_config('granted_patent', schedule='quarterly', **{"execution_date": datetime.date(2023, 1, 1)})
+    config = get_current_config('patent', schedule='quarterly', **{"execution_date": datetime.date(2023, 12, 31)})
+    print(config['PATENTSVIEW_DATABASES']["ELASTICSEARCH_DB"])
     qc = ElasticDBTester(config)
     qc.runReportingTests()
