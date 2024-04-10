40ab63a7ed5830d8e1fda8e168f0e5a1a98c4909
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2024-04-10 13:38:10 -0400

    fixing deletion sql query

--- reporting_database_generator/00_Deletion.sql
+++ reporting_database_generator/00_Deletion.sql
@@ -1,9 +1,9 @@
 {% set prior_quarter_date = macros.ds_format(macros.ds_add(dag_run.data_interval_start | ds, -1), "%Y-%m-%d", "%Y%m%d") %}
 {% set reporting_db = "PatentsView_" + prior_quarter_date %}
 {% set elastic_production_patent_db = "elastic_production_patent_" + prior_quarter_date %}
-{% set elastic_production_pgpubs_db = "elastic_production_pgpubs_" + prior_quarter_date %}
+{% set elastic_production_pgpub_db = "elastic_production_pgpub_" + prior_quarter_date %}
 
 
-drop database `{{elastic_production_patent_db}}` ;
-drop database `{{elastic_production_pgpubs_db}}` ;
+# drop database `{{elastic_production_patent_db}}` ;
+drop database `{{elastic_production_pgpub_db}}` ;
 drop database `{{reporting_db}}` ;
