42626a33aaa41e9894ceb07d8e47535f15f744cc
Jack Butler <jbutler@air.org>
2024-10-18 15:20:20 -0400

    add elastic staging DB views for text tables

---
+++ reporting_database_generator/elastic_scripts/13_02_g_claim_increment.sql
@@ -0,0 +1,23 @@
+{% set elastic_db = "elastic_production_patent_" + macros.ds_format(macros.ds_add(dag_run.data_interval_end | ds, -1), "%Y-%m-%d", "%Y%m%d") %}
+{% set update_end_date = macros.ds_format(macros.ds_add(dag_run.data_interval_end | ds, -1), "%Y-%m-%d", "%Y%m%d") %}
+{% set update_start_date = macros.ds_format(macros.ds_add(dag_run.data_interval_start | ds, -1), "%Y-%m-%d", "%Y%m%d") %}
+{% set update_year = macros.ds_format(macros.ds_add(dag_run.data_interval_end | ds, -1), "%Y-%m-%d", "%Y") %}
+
+use `{{elastic_db}}`;
+
+create or replace sql security invoker view `{{elastic_db}}`.g_claim_delta as
+select
+    uuid,
+    patent_id,
+    claim_sequence,
+    claim_text,
+    `dependent` as claim_dependent,
+    claim_number,
+    exemplary,
+    version_indicator as document_date
+
+
+from
+    `patent_text`.`claim_{{update_year}}` pt
+
+where pt.version_indicator BETWEEN '{{update_start_date}}' and '{{update_end_date}}'
