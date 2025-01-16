53d59060954bd4c6a986735cc9d1dc9d2daa3cbf
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2025-01-16 11:52:04 -0500

    trying to fix implicit type conversion

--- reporting_database_generator/elastic_scripts/10_02_patent_inventor.sql
+++ reporting_database_generator/elastic_scripts/10_02_patent_inventor.sql
@@ -6,17 +6,17 @@ use `{{elastic_db}}`;
 
 CREATE TABLE IF NOT EXISTS `{{elastic_db}}`.`patent_inventor`
 (
-    `inventor_id`            int(10) unsigned                       NOT NULL,
+    `inventor_id`            varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     `persistent_inventor_id` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
     `patent_id`              varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `sequence`               smallint(5) unsigned                   NOT NULL,
+    `sequence`               varchar(50)  COLLATE utf8mb4_unicode_ci NOT NULL,
     `name_first`             varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `name_last`              varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `gender_code`            varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `gender_code`            varchar(5)  COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
     `city`                   varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `state`                  varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `country`                varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `location_id`            int(10) unsigned                       DEFAULT NULL,
+    `state`                  varchar(256) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
+    `country`                varchar(256) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
+    `location_id`            varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
     `persistent_location_id` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
     PRIMARY KEY (`patent_id`, `inventor_id`, `sequence`),
     KEY `ix_inventor_name_first` (`name_first`),
@@ -41,7 +41,7 @@ select pi.inventor_id
      , l.country
      , pi.location_id
      , timi.old_inventor_id AS persistent_inventor_id
-     , timl.old_location_id AS persistent_location_id
+     , timl.old_location_id  AS persistent_location_id
 from `{{reporting_db}}`.patent_inventor pi
          join `{{reporting_db}}`.inventor i on i.inventor_id = pi.inventor_id
          join `{{reporting_db}}`.temp_id_mapping_inventor timi on timi.new_inventor_id = i.inventor_id
@@ -49,4 +49,3 @@ from `{{reporting_db}}`.patent_inventor pi
          left join `{{reporting_db}}`.location l on l.location_id = pi.location_id
          left join `{{reporting_db}}`.temp_id_mapping_location timl on timl.new_location_id = l.location_id
          join `{{elastic_db}}`.patents p on p.patent_id = pi.patent_id;
-
