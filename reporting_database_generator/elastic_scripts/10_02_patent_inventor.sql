c35d62d9ec7f86c2a2c205ebc8a17a942b637c69
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2025-01-17 12:55:31 -0500

    removing extra join

--- reporting_database_generator/elastic_scripts/10_02_patent_inventor.sql
+++ reporting_database_generator/elastic_scripts/10_02_patent_inventor.sql
@@ -6,17 +6,17 @@ use `{{elastic_db}}`;
 
 CREATE TABLE IF NOT EXISTS `{{elastic_db}}`.`patent_inventor`
 (
-    `inventor_id`            varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `persistent_inventor_id` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `patent_id`              varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `sequence`               varchar(50)  COLLATE utf8mb4_unicode_ci NOT NULL,
+    `inventor_id`            int(10) unsigned                       NOT NULL,
+    `persistent_inventor_id` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `patent_id`              varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `sequence`               int(11)                                NOT NULL,
     `name_first`             varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `name_last`              varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `gender_code`            varchar(5)  COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
+    `gender_code`            varchar(10)  COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
     `city`                   varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `state`                  varchar(256) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
-    `country`                varchar(256) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
-    `location_id`            varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `state`                  varchar(20) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
+    `country`                varchar(10) COLLATE utf8mb4_unicode_ci  DEFAULT NULL,
+    `location_id`            int(11)                                 DEFAULT NULL,
     `persistent_location_id` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     PRIMARY KEY (`patent_id`, `inventor_id`, `sequence`),
     KEY `ix_inventor_name_first` (`name_first`),
@@ -35,17 +35,16 @@ select pi.inventor_id
      , pi.sequence
      , i.name_first
      , i.name_last
-     , g.gender_flag
+     , i.gender_flag
      , l.city
      , l.state
      , l.country
      , pi.location_id
-     , timi.old_inventor_id AS persistent_inventor_id
-     , timl.old_location_id  AS persistent_location_id
+     , timi.old_inventor_id
+     , timl.old_location_id
 from `{{reporting_db}}`.patent_inventor pi
          join `{{reporting_db}}`.inventor i on i.inventor_id = pi.inventor_id
          join `{{reporting_db}}`.temp_id_mapping_inventor timi on timi.new_inventor_id = i.inventor_id
-         left join gender_attribution.inventor_gender_{{version_indicator}} g on pi.inventor_id = g.inventor_id
          left join `{{reporting_db}}`.location l on l.location_id = pi.location_id
          left join `{{reporting_db}}`.temp_id_mapping_location timl on timl.new_location_id = l.location_id
          join `{{elastic_db}}`.patents p on p.patent_id = pi.patent_id;
