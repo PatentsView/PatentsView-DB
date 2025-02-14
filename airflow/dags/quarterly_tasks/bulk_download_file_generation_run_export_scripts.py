fc2f5fad568a337f6295469fdc7e15b2d6cef253
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2025-02-14 12:56:50 -0500

    first attempt at automating bulk download file generation

--- airflow/dags/quarterly_tasks/bulk_download_file_generation_run_export_scripts.py
+++ airflow/dags/quarterly_tasks/bulk_download_file_generation_run_export_scripts.py
@@ -23,7 +23,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=1),
     'concurrency': 40,
     'queue': 'disambiguation'
 }
