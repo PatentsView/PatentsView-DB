f44954524e79fc020ae70e9ed49bbfb45f07f73c
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2025-02-14 12:48:42 -0500

    first attempt at automating bulk download file generation

--- airflow/dags/quarterly_tasks/bulk_download_file_generation_run_export_scripts.py
+++ airflow/dags/quarterly_tasks/bulk_download_file_generation_run_export_scripts.py
@@ -72,7 +72,7 @@ with DAG(
         default_args=default_args,
         # start_date=datetime(2025, 1, 1),
         start_date=days_ago(1),
-        schedule_interval="*/5 * * * *",
+        schedule_interval='@daily',
         catchup=True,
         template_searchpath="/project/bulk_download_file_generation/"
 ) as dag:
