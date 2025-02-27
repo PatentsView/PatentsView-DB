2dc7fcc42cae08ffe86d2910a4c87bd90dcb2cb7
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2025-02-27 11:52:28 -0500

    new attempt with PV-Downloads on PVGP2 server

--- airflow/dags/quarterly_tasks/bulk_download_file_generation_run_export_scripts.py
+++ airflow/dags/quarterly_tasks/bulk_download_file_generation_run_export_scripts.py
@@ -40,7 +40,7 @@ def verify_directory_contents(**kwargs):
         print(f)
 
     # Change directory one level up (parent directory)
-    config_dir = os.path.join(project_home,"..", "scripts")  # Explicitly move up one directory
+    config_dir = os.path.join(project_home,"..")  # Explicitly move up one directory
     print(f"Attempting to change into: {config_dir}")
 
     try:
