4cae74c7a508372cc838e8ca4dc845dc760844e1
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2025-02-27 12:47:40 -0500

    new attempt with PV-Downloads on PVGP2 server

--- airflow/dags/quarterly_tasks/bulk_download_file_generation_run_export_scripts.py
+++ airflow/dags/quarterly_tasks/bulk_download_file_generation_run_export_scripts.py
@@ -92,6 +92,7 @@ def create_copy_json_tasks(json_files, config_dir):
         copy_task = BashOperator(
             task_id=f"copy_{file_name}_json",
             bash_command=f"""
+                cd {config_dir} && \
                 cp {file_name}.json {file_name}_temp_25.json
             """
         )
