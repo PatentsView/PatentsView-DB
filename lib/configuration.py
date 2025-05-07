6c1c1e746a00c95aa465c3f9e1267d61f1330c08
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2025-05-07 15:18:48 -0400

    trying new file path

--- lib/configuration.py
+++ lib/configuration.py
@@ -288,11 +288,6 @@ def get_current_config(type='granted_patent', schedule='weekly', **kwargs):
 
     prefixed_string = f"{config_prefix}{temp_date}"
 
-    # Set working folder (base)
-    config['FOLDERS']["WORKING_FOLDER"] = "{data_root}/{prefix}".format(
-        prefix=prefixed_string,
-        data_root=config['FOLDERS']['data_root'])
-
     # Conditional configs
     if type == 'granted_patent':
         config['FOLDERS']['granted_patent_bulk_xml_location'] = '{working_folder}/raw_data/'.format(
@@ -315,7 +310,6 @@ def get_current_config(type='granted_patent', schedule='weekly', **kwargs):
         }
 
     elif type == 'pgpubs':
-        config['FOLDERS']["pgpubs_bulk_xml_location"] = "/app-volume/pgpubs/xml_files/"  # Or make dynamic if desired
 
         config['PATENTSVIEW_DATABASES'].update({
             "TEMP_UPLOAD_DB": prefixed_string,
@@ -325,6 +319,10 @@ def get_current_config(type='granted_patent', schedule='weekly', **kwargs):
             "ELASTICSEARCH_DB": f"elastic_production_pgpub_{temp_date}",
             "ELASTICSEARCH_DB_TYPE": "elasticsearch_pgpub"
         })
+
+        config['FOLDERS']["WORKING_FOLDER"] = "{data_root}/{prefix}".format(
+            prefix=prefixed_string,
+            data_root=config['FOLDERS']['data_root'])
 
         config['USPTO_LINKS'] = {
             "product_identifier": "APPXML",
